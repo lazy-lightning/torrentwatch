990857f21c722413af9906ea0ca12eb6813def59
journey4712@yahoo.com <journey4712@yahoo.com@84144212-d151-0410-ac56-97821e5b5ab9>
2008-08-15 10:56:08 +0000

    * move work functions from torrentwatch.php to rss_dl_utils.php  * call torrentwatch functions directly rather than loading new parser in update_btcli()

--- branches/tw-php5/rss_dl_utils.php
+++ branches/tw-php5/rss_dl_utils.php
@@ -192,7 +192,11 @@
 				return;
 			if(isset($config_values['Settings']['Cache Dir'])) {
 				_debug("Enabling Cache\n", 2);
-				mkdir($config_values['Settings']['Cache Dir'], 777, TRUE);
+				if(!file_exists($config_values['Settings']['Cache Dir']) ||
+				  	!is_dir($config_values['Settings']['Cache Dir'])) {
+					unlink($config_values['Settings']['Cache Dir']);
+					mkdir($config_values['Settings']['Cache Dir'], 777, TRUE);
+				}
 			}
 		}
 
@@ -298,7 +302,13 @@
 
 		function update_btcli()
 		{
-			global $config_values;
+			global $config_values, $hit;
+			$hit = 0;
+			check_for_torrents($config_values['Settings']['Watch Dir'], $config_values['Settings']['Download Dir']);
+			if(!$hit)
+				_debug("No New Torrents to add\n", 0);
+			return;
+			// OLD WAY
 			_debug("Running BTCLI Update Program\n");
 			exec('/share/.torrents/torrentwatch.php check', $output);
 			if(isset($config_values['Global']['HTMLOutput']))
@@ -444,9 +454,17 @@ function btcli_html($output) {
 define('FEED_UNKNOWN', 0);
 define('FEED_ATOM', 1);
 define('FEED_RSS', 2);
-function guess_feedtype($feedfile) {
-
-	$content = file($feedfile);
+function guess_feedtype($feedurl) {
+	global $config_values;
+	// First try the Cache
+	if(isset($config_values['Settings']['Cache Dir'])) {
+		if(file_exists($config_values['Settings']['Cache Dir'].'/rsscache_' . md5($feedurl)))
+			return FEED_RSS;
+		if(file_exists($config_values['Settings']['Cache Dir'].'/atomcache_'.md5($feedurl)))
+			return FEED_ATOM;
+	}
+	// Otherwise load it directly
+	$content = file($feedurl);
 	for($i = 0;$i < count($content);$i++) {
 		if(preg_match('/<feed xml/', $content[$i], $regs))
 			return FEED_ATOM;
@@ -490,5 +508,72 @@ function transmission_get_dl_dir() {
 			$capture = 1;
 	}
 	return Null;
+}
+function client_add_torrent($directory, $filename, $dest) {
+	global $config_values, $hit;
+	$btcli = '/mnt/syb8634/bin/btcli';
+	$btcli_add = 'add -d';
+	$btcli_connect='-d /opt/sybhttpd/localhost.drives/HARD_DISK/.btpd/';
+	$btcli_exec="$btcli $btcli_connect";
+
+	$trans_remote = '/mnt/syb8634/bin/transmission-remote';
+	$trans_connect = '-g /share/.transmission';
+	$trans_exec = "$trans_remote $trans_connect";
+	$trans_down_dir = '-f';
+	$trans_add = '-a';
+
+	$hit = 1;
+	if($config_values['Settings']['Deep Directories']) {
+		preg_match("/(.*)\.torrent/", $filename, $matches);
+		$orig = $dest;
+		$dest = "$dest/$matches[1]";
+		_debug("Deep Directorys, change dest to $dest\n", 1);
+	}
+	if(!file_exists($dest) or !is_dir($dest)) {
+		unlink($dest);
+		mkdir($dest, 777, TRUE);
+	}
+	if($config_values['Settings']['Client'] == "btpd")
+		exec("$btcli_exec $btcli_add \"$dest\" \"$directory/$filename\"", $output, $return);
+	else if($config_values['Settings']['Client'] == "transmission") {
+		/* Transmission wont let us explicitly set download dir till they release 1.30 */
+		$trans_dl = transmission_get_dl_dir();
+		if(!($trans_dl == $dest))
+			exec("$trans_exec $trans_down_dir \"$dest\"");
+		exec("$trans_exec $trans_add \"$directory/$filename\"", $output, $return);
+		/* sometimes our download ends up here anyways, depending on when the file gets written to disk */
+		if(!($trans_dl == $dest))
+			exec("$trans_exec $trans_down_dir \"$trans_dl\"");
+	} else {
+		_debug("Invalid Torrent Client: ".$config_values['Settings']['Client']."\n",0);
+		exit(1);
+	}
+	if($return == 0)
+		_debug("Starting: $filename in $dest\n",0);
+	else 
+		_debug("Failed Starting: $filename  Return code $return\n",0);
+	if($config_values['Global']['HTMLOutput']) {
+		if($return == 0)
+			echo("Starting: $filename in $dest<br>\n");
+		else
+			echo("Failed Starting $filename  Return code $return<br>\n");
+	}
+	if($config_values['Settings']['Save Torrents'])
+		rename("$directory/$filename", "$dest/".basename($filename));
+	else
+		unlink("$directory/$filename");
+}
+
+function check_for_torrents($directory, $dest) {
+	if($handle = opendir($directory)) {
+		while(false !== ($file = readdir($handle))) {
+			if(preg_match('/\.torrent$/', $file))
+				client_add_torrent($directory, $file, $dest);
+		}
+		closedir($handle);
+	} else {
+		_debug("check_for_torrents: Couldn't read Directory: $directory\n", 0);
+		exit(1);
+	}
 }
 ?>
