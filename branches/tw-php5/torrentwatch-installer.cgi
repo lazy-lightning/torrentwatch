b33dbe2e7328a263493b98d19ba3b7fb9fd24460
journey4712@yahoo.com <journey4712@yahoo.com@84144212-d151-0410-ac56-97821e5b5ab9>
2008-08-15 03:54:26 +0000

    First round of updates to php5  * Modified .php scripts to call php5-cgi, and apropriate cmd line options  * Modified tw-iface to use $_GET[] instead of custom parsing  * Removed contents of myurlencode()  * Modified fetch_http() to use file_[get|put]_contents when not using wget  * Modified cache_setup() to use mkdir() instead of exec("mkdir");  * Removed backwards compatible file_put_contents from rss_dl_utils.php  * Formatting cleanups in rss_dl_utils.php Other Changes  * Modified show_torrent_html() to offer download link on all status types

---
+++ branches/tw-php5/torrentwatch-installer.cgi
@@ -0,0 +1,274 @@
+#!/bin/sh 
+
+# Get the header in early, incase we have any errors
+cat <<EOF
+Content-Type: text/html
+
+<html>
+<head>
+<META HTTP-EQUIV="expires" CONTENT="0">
+<title>
+telnet-installer
+</title>
+</head>
+<body>
+
+<h2>
+EOF
+
+# Clean up PATH for security reasons.
+PATH=/bin:/usr/bin
+
+# Work out where we are run from.
+pwd=$0;
+# "/opt/sybhttpd/localhost.drives/HARD_DISK/llink-installer.cgi"
+urlpwd=`echo $pwd | sed 's#/opt/sybhttpd/localhost.drives/\(.*\)/.*$#\1#g'`
+# "HARD_DISK"
+exename=`echo $pwd | sed 's#.*/\([^/]*\)$#\1#g'`
+# "torrentwatch-installer.cgi"
+
+
+# Defines used for this version
+INSTALL="/opt/sybhttpd/localhost.drives/$urlpwd/tw.scripts.tar"
+DEST="/opt/sybhttpd/localhost.drives/HARD_DISK/.torrents"
+USBDIR="/opt/sybhttpd/localhost.drives/$urlpwd"
+STARTER="/opt/sybhttpd/localhost.drives/HARD_DISK/start_app.sh"
+# This is where our php interface script will be linked to
+# cannot be in a hidden directory like .torrents
+IFACE_DEST="/share/"
+IMAGE_DEST="/share/images/"
+
+# Script to install into the autostarter
+START_SCRIPT='rss_dl.php'
+SCRIPT_OPTS='-i'
+
+
+###############################################################
+# Define the functions we will call.
+# No changes should be needed from here on
+###############################################################
+MARKER="#M_A_R_K_E_R_do_not_remove_me"
+FTPSERVER="/mnt/syb8634/etc/ftpserver.sh"
+
+# Send the closing HTML
+end_html() 
+{ 
+  cat <<EOF
+</h2>
+</body>
+</html>
+EOF
+  
+}
+
+
+# Do some minor sanity checks that the HDD is mounted.
+check_has_hdd()
+{
+
+  /bin/mount | /bin/grep -q /opt/sybhttpd/localhost.drives/HARD_DISK
+  if [ $? != 0 ]; then
+    echo "Sorry, but you do not appear to have HARD_DISK mounted<br>" 
+    end_html
+    exit 0
+  fi
+
+}
+
+autostart_add()
+{   
+    /bin/cat "$STARTER" | /bin/grep -q "$START_SCRIPT"
+    if [ $? == 0 ]; then
+		echo "$START_SCRIPT already set to start on boot, skipping <br>"
+    else
+		echo "Adding $START_SCRIPT to community agreed startup script.<br>"
+		
+		rm -f /tmp/.starter.tmp
+		IFS=""
+		cat "$STARTER" | while read line 
+		do
+		  echo "$line" >> /tmp/.starter.tmp
+		  if [ x"$line" == x"$MARKER" ]; then
+			  # echo "cd ${DEST}/ && ./telnetd -l /bin/sh &" >> /tmp/.starter
+			  echo "${DEST}/${START_SCRIPT} ${SCRIPT_OPTS}" >> /tmp/.starter.tmp
+		  fi
+		done
+		cat < /tmp/.starter.tmp > "$STARTER"
+		chmod 755 "$STARTER"
+		rm -f /tmp/.starter.tmp
+    fi
+
+}
+
+autostart_setup()
+{
+
+  # Insert code here to make it auto-start
+	if [ -f "$STARTER" ]; then
+		echo "Found user community agreed on startup file...<br>"
+	else
+		# Create the file. Then do the scary part of making it be
+		# executed at boot time.
+		echo "Created user community startup file...<br>"
+		cat > "$STARTER" <<EOF
+#!/bin/sh
+#
+
+$MARKER
+
+exit 0
+EOF
+		chmod 755 "$STARTER"
+     fi
+
+		# Check if it is already called from ftpserver.sh
+	grep -q "$STARTER" "$FTPSERVER"
+	if [ $? != 0 ]; then
+		echo "Installer starter<br>"
+
+		cp "$FTPSERVER" "$FTPSERVER.backup"
+		
+		rm -f /tmp/.ftpserver.tmp /tmp/.found
+		IFS=""
+		cat "$FTPSERVER" | while read line
+		  do
+		  echo "$line" >> /tmp/.ftpserver.tmp
+		  if [ x"$line" == x"start() {" ]; then
+			  echo "      $STARTER &" >> /tmp/.ftpserver.tmp
+			  touch /tmp/.found
+		  fi
+		done
+		
+		if [ -f /tmp/.found ]; then
+			cat < /tmp/.ftpserver.tmp > "$FTPSERVER"
+			chmod 774 "$FTPSERVER"
+		fi
+		rm -f /tmp/.ftpserver.tmp /tmp/.found
+	fi
+	
+}
+
+# Install program on HDD
+install_harddisk()
+{ 
+	check_has_hdd
+
+	# find a name
+	set -- $INSTALL
+	INSTA="$1"
+
+ # We expect to find tarball next to this script.
+	if [ ! -f "$INSTA" ]; then
+		echo "Unable to read<br>"
+		echo "$INSTALL<br>"
+		echo "Please place binary next to<br>"
+		echo "$exename<br>"
+		echo "script, and try again.<br>"
+		exit 0;
+	fi
+
+	echo "Installing to HARD_DISK...<br>"
+  # We make sure that HARD_DISK/.torrents
+	mkdir -p "$DEST"
+	chmod 777 "$DEST"
+	
+	# Actually copy the data now.
+	(cd "$DEST" && tar -xf "$INSTA")
+	#cp -f "$INSTA" "$DEST/"
+
+	if [ $? != 0 ]; then
+		echo "Sorry, it appears that tar returned an error.<br>"
+		exit 0;
+	fi
+
+	echo "Configuring installed files.<br>"
+	# Verify our new program in executable
+	chmod a+x $DEST/$START_SCRIPT
+	chmod a+x $DEST/torrentwatch.php
+	chmod a+x $DEST/tw-iface.php
+
+	# Link our configuration script to the HARD_DRIVE
+	# Necessary because the PCH web server wont let us access hidden folders like .torrents
+	if [ ! -h $IFACE_DEST/tw-iface.cgi ]; then
+		rm $IFACE_DEST/tw-iface.cgi
+		ln -s $DEST/tw-iface.php $IFACE_DEST/tw-iface.cgi
+	fi
+	if [ ! -h $IFACE_DEST/tw-iface.local.css ]; then
+		rm $IFACE_DEST/tw-iface.local.css
+		ln -s $DEST/tw-iface.local.css $IFACE_DEST/tw-iface.local.css
+	fi
+	if [ ! -h $IFACE_DEST/tw-iface.css ]; then
+		rm $IFACE_DEST/tw-iface.css
+		ln -s $DEST/tw-iface.css $IFACE_DEST/tw-iface.css
+	fi
+	# Move the image files where they need to be for the interface
+	mkdir -p $IMAGE_DEST
+	mv $DEST/images/* $IMAGE_DEST
+	rmdir $DEST/images
+
+	# Anti-clobber routine for the config script
+	if [ ! -f $DEST/rss_dl.config ];then
+		cp $DEST/rss_dl.config.orig $DEST/rss_dl.config
+	fi
+
+	# Check auto starter
+	autostart_setup
+	
+	# Add our script to the auto starter
+	echo "<p>Installing ${START_SCRIPT} into ${STARTER}</p>"
+	autostart_add
+	# Run the script, since the autostart wont be running until reboot
+	date >> /var/rss_dl.log
+	echo "Installed cron hook from configuration script" >> /var/rss_dl.log
+	${DEST}/${START_SCRIPT} ${SCRIPT_OPTS}
+
+	echo "Success..<br>"
+
+	echo 'Please procede to the';
+	echo '<a href="http://localhost.drives:8883/HARD_DISK/tw-iface.cgi">Configuration Interface</a>.<br>'
+  # Remove the tarball
+	rm -f "$INSTA"
+
+	exit 0;
+}
+
+
+
+
+
+#########################################################################
+#
+# Main function begins here
+#
+#########################################################################
+
+#start_html
+
+
+# check if we were run without arguments, if so display "menu"
+case "$1" in
+  install)
+      install_harddisk;;
+esac
+
+
+cat <<EOF
+Welcome to the Torrent Watch Folder installer.<br>
+We are currently running from:<br>
+$urlpwd/$exename<br>
+<br>
+
+<a href="http://localhost.drives:8883/$urlpwd/$exename?install">
+Install to HARD_DISK
+</a>
+
+<br>
+
+
+EOF
+
+end_html
+
+exit 0
+
+
---
+++ branches/tw-php5/tw-iface.php
@@ -0,0 +1,226 @@
+#!/mnt/syb8634/server/php5-cgi
+<?php
+
+ini_set('include_path', '.:/share/.torrents');
+$config_file = '/share/.torrents/rss_dl.config';
+
+require_once('rss_dl_utils.php');
+
+function parse_options() {
+	global $html_out, $config_values;
+	$filler = "<br>";
+	$exit = True;
+	$output = "";
+
+	if(isset($_GET['mode'])) {
+		if($_GET['mode'] == 'viewlog') {
+			$exec = 'cat /var/rss_dl.log';
+		} else if($_GET['mode'] == 'dlnow') {
+			$filler = "\n";
+			$exec = '/share/.torrents/rss_dl.php -v -D -H';
+		} else if($_GET['mode'] == 'emptycache') {
+			$exec = "rm -f ".$config_values['Settings']['Cache Dir']."/*";
+			$exit = False;
+		} else if($_GET['mode'] == 'test') {
+			$filler = "\n";
+			$exec = '/share/.torrents/rss_dl.php -v -H -t';
+		} else if($_GET['mode'] == 'del') {
+			$exec = '/opt/sybhttpd/localhost.drives/HARD_DISK/.torrents/rss_dl.php -v -r "'.urldecode($_GET['rss']).'" "'.$_GET['key'].'" "'.$_GET['data'].'"';
+			$exit = False;
+		} else if ($_GET['mode'] == 'add') {
+			$exec = '/opt/sybhttpd/localhost.drives/HARD_DISK/.torrents/rss_dl.php -v -a "'.$_GET['rss'].'" "'.$_GET['key'].'" "'.$_GET['data'].'"';
+			$exit = False;
+		} else if ($_GET['mode'] == 'setglobals') {
+			$config_values['Settings']['Download Dir']=urldecode($_GET['downdir']);
+			$config_values['Settings']['Watch Dir']=urldecode($_GET['watchdir']);
+			$config_values['Settings']['Deep Directories']=(isset($_GET['deepdir']) ? 1 : 0);
+			$config_values['Settings']['Verify Episode']=(isset($_GET['verifyepisodes']) ? 1 : 0);
+			$config_values['Settings']['Save Torrents']=(isset($_GET['savetorrents']) ? 1 : 0);
+			write_config_file();
+			$exit = False;
+		} else if ($_GET['mode'] == 'matchtitle') {
+			if(($tmp = guess_match(html_entity_decode($_GET['title']))))
+				$exec = '/opt/sybhttpd/localhost.drives/HARD_DISK/.torrents/rss_dl.php -v -a "'.$_GET['rss'].'" "'.$tmp['key'].'" "'.$tmp['data'].'"';
+			else
+				$exec = 'echo Could not generate Match';
+			$exit = False;
+		} else  if ($_GET['mode'] == 'dltorrent') {
+			global $config_values;
+			if(isset($config_values['Settings']['Cache Dir']))
+				$old = $config_values['Settings']['Cache Dir'];
+			unset($config_values['Settings']['Cache Dir']);
+			echo("<div class='execoutput'>");
+			echo("Fetching ".trim(urldecode($_GET['title']))." from ".trim(urldecode($_GET['link']))."<br>");
+			fetch_torrent(trim(urldecode($_GET['title'])), trim(urldecode($_GET['link'])));
+			update_btcli();
+			echo("</div>");
+			if(isset($old))
+				$config_values['Settings']['Cache Dir'] = $old;
+			$exit = False;
+			//$html_out .= $html_header;
+		} else {
+			$html_out .= "<h2>Bad Paramaters passed to tw-iface.php</h2>";
+		}
+	}
+
+	if(isset($exec)) {
+		exec($exec, $output);
+		$html_out .= "<div class='execoutput'>".implode($filler, $output)."</div>";
+		echo($html_out);
+		$html_out = "";
+	}
+	if($exit) {
+		$html_out .= "<div class='clear'></div>\n</div></body></html>\n";  
+		echo($html_out);
+		exit(0);
+	}
+	return;
+}
+
+function display_form($rss = FALSE) {
+	global $html_out;
+	$html_out .= '<form action="tw-iface.cgi"><input type="hidden" name="mode" value="add">';
+	if($rss) {
+		$html_out .= '<input type="hidden" name="rss" value="'.$rss.'">';
+	} else {
+		$html_out .= '<tr><td colspan=3>&nbsp;</td></tr>';
+		$html_out .= '<tr style="text-align: center"><td colspan="2">';
+		$html_out .= 'New RSS Feed</td></tr><tr><td colspan="2">';
+		$html_out .= '<center><label>Feed: <input type="text" name="rss"></center></td></tr>';
+	} 
+	$html_out .= '<tr><td>';
+	$html_out .= '<input type="text" name="key"></td><td><input type="text" name="data"></td>';
+	$html_out .= '<td><input type="image" src="images/add.png" name="optional"></td>';
+	if($rss) {	
+		$html_out .= '<tr><td class="feedlink" colspan="3">&nbsp;</td></tr>';
+		$html_out .= '<tr><td><br />&nbsp;<br /></td></tr>';
+	}
+	$html_out .= '</form>';
+}
+
+function display_global_settings() {
+	global $config_values, $html_out;
+	
+	$html_out .= '<tr><td colspan=2>&nbsp;</td></tr>';
+  $html_out .= '<form action="tw-iface.cgi"><input type="hidden" name="mode" value="setglobals">';
+	$html_out .= '<tr><td colspan=2 style="text-align: center;">Global Settings ';
+	$html_out .= '<input type="image" src="images/add.png" name="optional"></td></tr>';
+
+	$html_out .= '<tr>';
+	$html_out .= '<td style="text-align: right;">Download Directory:</td>';
+	$html_out .= '<td><input type="text" name="downdir" value='.$config_values['Settings']['Download Dir'].'></td>';
+	$html_out .= '</td></tr>';
+
+	$html_out .= '<tr><td style="text-align: right;">Watch Directory:</td>';
+	$html_out .= '<td><input type="text" name="watchdir" value='.$config_values['Settings']['Watch Dir'].'></td>';
+	$html_out .= '</td></tr>';
+
+
+	$html_out .= '<tr><td style="text-align: right;">Save .torrent:</td>';
+	$html_out .= '<td><input type="checkbox" name="savetorrents" value=1 ';
+	if($config_values['Settings']['Save Torrents'] == 1)
+		$html_out .= 'checked=1';
+	$html_out .= '></td></tr>';
+
+	$html_out .= '<tr><td style="text-align: right;">Deep Directories:</td>';
+	$html_out .= '<td><input type="checkbox" name="deepdir" value=1 ';
+	if($config_values['Settings']['Deep Directories'] == 1)
+		$html_out .= 'checked=1';
+	$html_out .= '></td></tr>';
+	$html_out .= '<tr><td style="text-align: right;">Verify Episodes:</td>';
+	$html_out .= '<td><input type="checkbox" name="verifyepisodes" value=1 ';
+	if($config_values['Settings']['Verify Episode'] == 1)
+		$html_out .= 'checked=1';
+	$html_out .= '></td></tr></form>';
+
+}
+	
+function display_config() {
+	global $config_values, $html_out;
+
+	function rss_callback($item, $key) {
+		global $config_values, $html_out;
+		$html_out .= "\n";
+		if(strcmp($key, 'Settings') == 0 or strcmp($key, 'Global') == 0)
+			return;
+		//$html_out .= '<tr><td colspan=3>&nbsp;</td></tr>';
+		$html_out .= '<tr><td class="feedlink" colspan="3">Feed: <a href="'.$key.'" target="_Blank">'.$key.'</a></td></tr>';
+		array_walk($config_values[$key], 'match_callback', $key);
+		display_form($key);
+	}
+
+	function match_callback($item, $key, $parent) {
+		global $config_values, $html_out;
+		$html_out .= "\n";
+		$new_rss = my_urlencode($parent);
+    
+		$html_out .= '<tr><td>'.$key.'</td><td>'.$item.'</td><td>';
+		$html_out .= '<form action="tw-iface.cgi">';
+		$html_out .= '<input type="hidden" name="mode" value="del">';
+		$html_out .= '<input type="hidden" name="rss" value="'.$new_rss.'">';
+		$html_out .= '<input type="hidden" name="key" value="'.$key.'">';
+		$html_out .= '<input type="hidden" name="data" value="'.$item.'">';
+		$html_out .= '<input type="image" src="images/del.png" name="optional">';
+		/* $html_out .= '<input class="del" type="submit" value="Delete">'; */
+		$html_out .= '</form></td></tr>'."\n";
+	}
+
+
+	$html_out .= '<div class="configuration"><table>';
+	$html_out .= '<tr style="text-align: center"><td colspan="3"><h2>Current Feed Configuration</h2></td></tr>';
+	array_walk($config_values, 'rss_callback');
+	display_form();
+	display_global_settings();
+	$html_out .= "</table></div>";
+}
+
+function display_options() {
+	global $html_out, $config_values;
+	$html_out .= '<div class="mainoptions"><ul>';
+	$html_out .= '<li id="config"><a href="tw-iface.cgi">Configure</a></li>';
+	$html_out .= '<li id="test"><a href="tw-iface.cgi?mode=test">Test Matches</a></li>';
+	$html_out .= '<li id="view"><a href="tw-iface.cgi?mode=viewlog">View log</a></li>';
+	$html_out .= '<li id="empty"><a href="tw-iface.cgi?mode=emptycache">Empty Cache</a></li>';
+	$html_out .= '<li id="dl"><a href="tw-iface.cgi?mode=dlnow">DL New Torrents</a></li>';
+	if($config_values['Settings']['Client'] == "btpd")
+		$html_out .= '<li id="webui"><a href="http://popcorn:8883/torrent/bt.cgi">BitTorrent WebUI</a></li>';
+	else if($config_values['Settings']['Client'] == "transmission")
+		$html_out .= '<li id="webui"><a href="http://popcorn:8077/index.php5">Clutch</a></li>';
+	$html_out .= '</ul></div>';
+	echo($html_out);
+	$html_out = "";
+}
+
+//
+//
+// MAIN Function
+//
+//
+
+echo ("<html><head><title>Torrentwatch Configuration Interface</title>");
+echo ("<meta http-equiv='expires' content='0'>");
+if($_SERVER["REMOTE_ADDR"] == '127.0.0.1')
+	echo ('<link rel="StyleSheet" type="text/css" href="tw-iface.local.css?'.time().'"></link>');
+else
+	echo ('<link rel="StyleSheet" type="text/css" href="tw-iface.css?'.time().'"></link>');
+echo ("</head><body><div class='container'>");
+$html_out = "";
+
+read_config_file();
+display_options();
+if(isset($_GET['mode'])) {
+	parse_options();
+	unset($config_values);
+	read_config_file();
+}
+display_config();
+
+
+$html_out .= "<div class='clear'></div>\n</div></body></html>\n";
+
+echo $html_out;
+
+exit(0);
+
+php?>
+
