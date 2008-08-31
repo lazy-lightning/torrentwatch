f490403908822325853d1e0e1be4588ec60fa3c2
journey4712@yahoo.com <journey4712@yahoo.com@84144212-d151-0410-ac56-97821e5b5ab9>
2008-08-31 04:18:10 +0000

    update buildRelease.sh with new files

--- branches/tw-php5/buildRelease.sh
+++ branches/tw-php5/buildRelease.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-PHP_FILES="atomparser.php lastRSS.php torrentwatch.php rss_dl.php rss_dl_utils.php class.bdecode.php rss_dl.functions.php tor_client.php"
+PHP_FILES="atomparser.php lastRSS.php torrentwatch.php rss_dl.php rss_dl_utils.php class.bdecode.php rss_dl.functions.php tor_client.php progressbar.php"
 DOC_FILES="TODO CREDITS LICENSE changelog rss_dl.config.orig"
 IMAGE_FILES="images/favicon.ico images/*.gif images/*.png"
-WEB_FILES="tw-iface.php webtoolkit.contextmenu.js tw-iface.css tw-iface.local.css tw-iface.js $IMAGE_FILES"
+WEB_FILES="tw-iface.php webappers.com.progress.js webtoolkit.contextmenu.js tw-iface.css tw-iface.local.css tw-iface.js $IMAGE_FILES"
 RELEASE_FILES="$PHP_FILES $DOC_FILES $WEB_FILES"
 RELEASE=torrentwatch-dev-$1
 
