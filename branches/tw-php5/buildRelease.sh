f4c65ed28c8873eff41e2fa5c5e4fd751dc2e3fd
journey4712@yahoo.com <journey4712@yahoo.com@84144212-d151-0410-ac56-97821e5b5ab9>
2008-11-13 11:30:31 +0000

    adjust patch inclusion

--- branches/tw-php5/buildRelease.sh
+++ branches/tw-php5/buildRelease.sh
@@ -2,12 +2,13 @@
 
 EXE_FILES="index.cgi rss_dl.php"
 DOC_FILES="changelog CREDITS LICENSE TODO"
-RELEASE_FILES="$EXE_FILES $DOC_FILES css/ images/ javascript/ php/ patches/"
+RELEASE_FILES="$EXE_FILES $DOC_FILES css/ images/ javascript/ php/ "
 RELEASE=torrentwatch-dev-$1
 
 rm -rf release
 mkdir -p release
 
+cp -rf patches/* release
 cp installer/* release
 tar -cf release/tw.scripts.tar --exclude-vcs $RELEASE_FILES
 
