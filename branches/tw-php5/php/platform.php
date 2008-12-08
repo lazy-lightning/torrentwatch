78ae1e022853cb5dfc1e204536771e8a3efe2ee8
somedude119@gmail.com <somedude119@gmail.com@84144212-d151-0410-ac56-97821e5b5ab9>
2008-12-08 07:55:28 +0000

    standardize indentation

--- branches/tw-php5/php/platform.php
+++ branches/tw-php5/php/platform.php
@@ -1,94 +1,94 @@
 <?php
 
 if(file_exists("/etc/init_nmt"))
-	$platform = "NMT";
+  $platform = "NMT";
 else if(is_dir("/home/xbmc"))
-	$platform = "XBMC";
+  $platform = "XBMC";
 else
-	$platform = "Linux";
+  $platform = "Linux";
 
 function platform_initialize() {
-	global $platform;
-	switch($platform) {
-		case 'NMT':
-			// PHP_SELF isn't properly set on the NMT
-			$_SERVER['PHP_SELF'] = $_SERVER['SCRIPT_NAME'];
-			break;
-	}
+  global $platform;
+  switch($platform) {
+    case 'NMT':
+      // PHP_SELF isn't properly set on the NMT
+      $_SERVER['PHP_SELF'] = $_SERVER['SCRIPT_NAME'];
+      break;
+  }
 }
 
 function platform_install() {
-	global $platform;
-	switch($platform) {
-		case 'NMT':
-			symlink_force(platform_getInstallRoot(), platform_getWebRoot()."/torrentwatch");
-			symlink_force("/mnt/syb8634/server/php5-cgi", "/usr/bin/php-cgi");
-			break;
-		case 'Linux':
-		default:
-			break;
-	}
+  global $platform;
+  switch($platform) {
+    case 'NMT':
+      symlink_force(platform_getInstallRoot(), platform_getWebRoot()."/torrentwatch");
+      symlink_force("/mnt/syb8634/server/php5-cgi", "/usr/bin/php-cgi");
+      break;
+    case 'Linux':
+    default:
+      break;
+  }
 }
-			
+      
 
 function platform_getConfigFile() {
-	return platform_getUserRoot()."/torrentwatch.config";
+  return platform_getUserRoot()."/torrentwatch.config";
 }
 
 function platform_getUserRoot() {
-	global $platform;
-	switch($platform) {
-		case 'NMT':
-			return "/share/.torrents";
-			break;
-		case 'XBMC':
-			return "/home/xbmc/.torrents";
-			break;
-		case 'Linux':
-		default:
-			return "~/.torrents";	
-			break;
-	}
+  global $platform;
+  switch($platform) {
+    case 'NMT':
+      return "/share/.torrents";
+      break;
+    case 'XBMC':
+      return "/home/xbmc/.torrents";
+      break;
+    case 'Linux':
+    default:
+      return "~/.torrents";  
+      break;
+  }
 }
 
 // this function is only called from platform_install()
 function platform_getWebRoot() {
-	global $platform;
-	switch($platform) {
-		case 'NMT':
-			return "/opt/sybhttpd/default/";
-			break;
-		case 'Linux':
-		default:
-			return "/var/www";
-			break;
-	}
-}	
+  global $platform;
+  switch($platform) {
+    case 'NMT':
+      return "/opt/sybhttpd/default/";
+      break;
+    case 'Linux':
+    default:
+      return "/var/www";
+      break;
+  }
+}  
 
 function platform_getDownloadDir() {
-	global $platform;
-	switch($platform) {
-		case 'NMT':
-			return "/share/Download";
-			break;
-		case 'Linux':
-		default:
-			return "~/Download";
-			break;
-	}
+  global $platform;
+  switch($platform) {
+    case 'NMT':
+      return "/share/Download";
+      break;
+    case 'Linux':
+    default:
+      return "~/Download";
+      break;
+  }
 }
 
 function platform_getInstallRoot() {
-	global $platform, $argv;
-	return dirname(realpath($argv[0]));
+  global $platform, $argv;
+  return dirname(realpath($argv[0]));
 }
 
 function platform_getTransmissionRemote() {
-	if(file_exists('/usr/bin/transmission-remote'))
-		return '/usr/bin/transmission/remote';
-	else if(file_exists('/mnt/syb8634/bin/transmission-remote'))
-		return '/mnt/syb8634/bin/transmission-remote';
-	return FALSE;
+  if(file_exists('/usr/bin/transmission-remote'))
+    return '/usr/bin/transmission/remote';
+  else if(file_exists('/mnt/syb8634/bin/transmission-remote'))
+    return '/mnt/syb8634/bin/transmission-remote';
+  return FALSE;
 }
 
 ?>
--- branches/tw-php5/php/torrentwatch.php
+++ branches/tw-php5/php/torrentwatch.php
@@ -6,26 +6,26 @@
 require_once('rss_dl_utils.php');
 
 function usage() {
-	global $argv;
-	echo "$argv[0] - Torrent Watch Folder\n";
-	echo "Valid Options\n";
-	echo "	check		 - check for new torrents\n\n";
+  global $argv;
+  echo "$argv[0] - Torrent Watch Folder\n";
+  echo "Valid Options\n";
+  echo "  check     - check for new torrents\n\n";
 }
 
 function parse_options() {
-	global $argc, $argv;
-	if($argc != 2) {
-		_debug("Wrong number of options passed to torrentwatch.php\n",0);
-		usage();
-		exit(1);
-	}
-	switch($argv[1]) {
-		case 'check';
-			break;
-		default:
-			usage();
-			exit(1);
-	}
+  global $argc, $argv;
+  if($argc != 2) {
+    _debug("Wrong number of options passed to torrentwatch.php\n",0);
+    usage();
+    exit(1);
+  }
+  switch($argv[1]) {
+    case 'check';
+      break;
+    default:
+      usage();
+      exit(1);
+  }
 }
 
 //
@@ -37,14 +37,14 @@ function parse_options() {
 read_config_file();
 parse_options();
 if(!isset($config_values['Settings']['Watch Dir']) or
-		!isset($config_values['Settings']['Download Dir'])) {
-	_debug("torrentwatch.php: Bad Config\n\n", 0);
-	exit(1);
+    !isset($config_values['Settings']['Download Dir'])) {
+  _debug("torrentwatch.php: Bad Config\n\n", 0);
+  exit(1);
 }
 $hit = 0;
 check_for_torrents($config_values['Settings']['Watch Dir'], $config_values['Settings']['Download Dir']);
 if(!$hit)
-	_debug("No New Torrents to add\n", 0);
+  _debug("No New Torrents to add\n", 0);
 exit(0);
 
 ?>
