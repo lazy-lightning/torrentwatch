038204bb7a3336812819d0dc9c94157f417a5cee
somedude119 <somedude119@84144212-d151-0410-ac56-97821e5b5ab9>
2009-01-15 11:00:45 +0000

    Release of torrentwatch 0.7.9

---
+++ tags/torrentwatch-dev-0.7.9/installer/torrentwatch-installer.cgi
@@ -0,0 +1,318 @@
+#!/bin/sh --
+# fix and run DOS format sh. ALord/Ydrol
+true # Keep blank below
+
+##################################
+if [ $? != 0 ]; then             #
+    set -e                       #
+    sed 's/$//' "$0" > /tmp/$$ #
+    cat /tmp/$$ > "$0"           #
+    rm -f /tmp/$$                #
+    exec /bin/sh "$0" "$@"       #
+fi                               #
+##################################
+# REAL SCRIPT FOLLOWS
+
+# Get the header in early, incase we have any errors
+cat <<EOF
+Content-Type: text/html
+
+<html>
+<head>
+<META HTTP-EQUIV="expires" CONTENT="0">
+<title>
+telnet-installer
+</title>
+</head>
+<body>
+
+<h2>
+EOF
+
+# Clean up PATH for security reasons.
+PATH=/bin:/usr/bin
+
+# Work out where we are run from.
+pwd=$0;
+# "/opt/sybhttpd/localhost.drives/HARD_DISK/llink-installer.cgi"
+urlpwd=`echo $pwd | sed 's#/opt/sybhttpd/localhost.drives/\(.*\)/.*$#\1#g'`
+# "HARD_DISK"
+exename=`echo $pwd | sed 's#.*/\([^/]*\)$#\1#g'`
+# "torrentwatch-installer.cgi"
+
+
+# Defines used for this version
+NAME="torrentwatch"
+CONFIG="$NAME.config"
+WEB_SCRIPT='index.cgi'
+
+# Script Defines
+INSTALL="/opt/sybhttpd/localhost.drives/$urlpwd/tw.scripts.tar"
+DEST="/opt/sybhttpd/localhost.drives/HARD_DISK/.torrents"
+USBDIR="/opt/sybhttpd/localhost.drives/$urlpwd"
+STARTER="/opt/sybhttpd/localhost.drives/HARD_DISK/start_app.sh"
+
+# Script to install into the autostarter
+START_SCRIPT='rss_dl.php'
+START_SCRIPT_OPTS='-i'
+
+###############################################################
+# Define the functions we will call.
+# No changes should be needed from here on
+###############################################################
+MARKER="#M_A_R_K_E_R_do_not_remove_me"
+FTPSERVER="/mnt/syb8634/etc/ftpserver.sh"
+PHP="/mnt/syb8634/server/php5-cgi"
+
+# Send the closing HTML
+end_html() 
+{ 
+	cat <<EOF
+</h2>
+</body>
+</html>
+EOF
+	
+}
+
+
+# Do some minor sanity checks that the HDD is mounted.
+check_has_hdd()
+{
+
+	/bin/mount | /bin/grep -q /opt/sybhttpd/localhost.drives/HARD_DISK
+	if [ $? != 0 ]; then
+		echo "Sorry, but you do not appear to have HARD_DISK mounted<br>" 
+		end_html
+		exit 0
+	fi
+
+}
+
+autostart_add()
+{	 
+	/bin/cat "$STARTER" | /bin/grep -q "$START_SCRIPT"
+	if [ $? == 0 ]; then
+		echo "$START_SCRIPT already set to start on boot, skipping <br>"
+	else
+		echo "Adding $START_SCRIPT to community agreed startup script.<br>"
+		
+		rm -f /tmp/.starter.tmp
+		IFS=""
+		cat "$STARTER" | while read line 
+		do
+			echo "$line" >> /tmp/.starter.tmp
+			if [ x"$line" == x"$MARKER" ]; then
+				# echo "cd ${DEST}/ && ./telnetd -l /bin/sh &" >> /tmp/.starter
+				echo "${DEST}/${START_SCRIPT} ${START_SCRIPT_OPTS}" >> /tmp/.starter.tmp
+			fi
+		done
+		cat < /tmp/.starter.tmp > "$STARTER"
+		chmod 755 "$STARTER"
+		rm -f /tmp/.starter.tmp
+	fi
+
+
+	/bin/cat "$STARTER" | /bin/grep -q "php-cgi"
+	if [ $? == 0 ]; then
+		echo "PHP already linked into place.<br>";
+	else
+		echo "Adding link to php in /usr/bin/php-cgi <br>"
+		rm -f /tmp/.starter.tmp
+		IFS=""
+		cat "$STARTER" | while read line 
+		do
+			echo "$line" >> /tmp/.starter.tmp
+			if [ x"$line" == x"$MARKER" ]; then
+				# echo "cd ${DEST}/ && ./telnetd -l /bin/sh &" >> /tmp/.starter
+				echo "ln -s ${PHP} /usr/bin/php-cgi" >> /tmp/.starter.tmp
+			fi
+		done
+		cat < /tmp/.starter.tmp > "$STARTER"
+		chmod 755 "$STARTER"
+		rm -f /tmp/.starter.tmp
+	fi
+}
+
+autostart_setup()
+{
+
+	# Insert code here to make it auto-start
+	if [ -f "$STARTER" ]; then
+		echo "Found user community agreed on startup file...<br>"
+	else
+		# Create the file. Then do the scary part of making it be
+		# executed at boot time.
+		echo "Created user community startup file...<br>"
+		cat > "$STARTER" <<EOF
+#!/bin/sh
+#
+
+PATH=\$PATH:/share/bin
+HOME=/share/
+
+$MARKER
+
+exit 0
+EOF
+		chmod 755 "$STARTER"
+	fi
+
+	# Check if it is already called from ftpserver.sh
+	grep -q "$STARTER" "$FTPSERVER"
+	if [ $? != 0 ]; then
+		echo "Installing auto-starter hook to $FTPSERVER"
+		cp "$FTPSERVER" "$FTPSERVER.backup"
+		
+		rm -f /tmp/.ftpserver.tmp /tmp/.found
+		IFS=""
+		cat "$FTPSERVER" | while read line
+			do
+			echo "$line" >> /tmp/.ftpserver.tmp
+			if [ x"$line" == x"start() {" ]; then
+				echo "			$STARTER &" >> /tmp/.ftpserver.tmp
+				touch /tmp/.found
+			fi
+		done
+		
+		if [ -f /tmp/.found ]; then
+			cat < /tmp/.ftpserver.tmp > "$FTPSERVER"
+			chmod 774 "$FTPSERVER"
+		fi
+		rm -f /tmp/.ftpserver.tmp /tmp/.found
+	fi
+	
+}
+
+# Install program on HDD
+install_harddisk()
+{ 
+	check_has_hdd
+
+	# find a name
+	set -- $INSTALL
+	INSTA="$1"
+
+	# We expect to find tarball next to this script.
+	if [ ! -f "$INSTA" ]; then
+		echo "Unable to read<br>"
+		echo "$INSTALL<br>"
+		echo "Please place binary next to<br>"
+		echo "$exename<br>"
+		echo "script, and try again.<br>"
+		exit 0;
+	fi
+
+	echo "Installing to HARD_DISK...<br>"
+
+	# Move the last install out of the way
+	if [ -d "$DEST" ]; then
+		if [ -d "$DEST.old" ]; then
+			rm -rf "$DEST.old"
+		fi
+		mv "$DEST" "$DEST.old"
+		mkdir "$DEST"
+		if [ -f "$DEST.old/rss_dl.config" ]; then
+			cp "$DEST.old/rss_dl.config"  "$DEST/torrentwatch.config"
+		fi
+		if [ -f "$DEST.old/torrentwatch.config" ]; then
+			cp "$DEST.old/torrentwatch.config" "$DEST"
+		fi
+		if [ -f "$DEST.old/rss_dl.history" ]; then
+			cp -rf "$DEST.old/rss_dl.history" "$DEST"
+		fi
+		if [ -d "$DEST.old/rss_cache" ]; then
+			cp -rf "$DEST.old/rss_cache" "$DEST"
+		fi
+	fi
+		
+	# We make sure that HARD_DISK/.torrents
+	mkdir -p "$DEST"
+	chmod 777 "$DEST"
+
+	# Actually copy the data now.
+	(cd "$DEST" && tar -xf "$INSTA")
+	#cp -f "$INSTA" "$DEST/"
+
+	if [ $? != 0 ]; then
+		echo "Sorry, it appears that tar returned an error.<br>"
+		exit 0;
+	fi
+
+	echo "Configuring installed files.<br>"
+	# Verify our new program in executable
+	chmod a+x $DEST/$START_SCRIPT
+	chmod a+x $DEST/$WEB_SCRIPT
+
+	# Remenants of an older version of torrentwatch
+	if [ -h /share/tw-iface.cgi ]; then
+		rm -f /share/tw-iface.cgi /share/tw-iface.local.css /share/tw-iface.css
+	fi
+
+	chown -R nmt.nmt $DEST
+
+	# Check auto starter
+	autostart_setup
+	
+	# Add our script to the auto starter
+	autostart_add
+	# Run the script, since the autostart wont be running until reboot
+	date >> /var/rss_dl.log
+	echo "Installed cron hook from configuration script" >> /var/rss_dl.log
+
+	if [ ! -h /usr/bin/php-cgi ]; then
+		rm -f /usr/bin/php-cgi
+		ln -s ${PHP} /usr/bin/php-cgi
+	fi
+	${DEST}/${START_SCRIPT} ${START_SCRIPT_OPTS}
+
+	echo "Success..<br>"
+
+	echo 'Please procede to the';
+	echo '<a href="http://popcorn:8883/torrentwatch/index.cgi">Configuration Interface</a> in a PC Browser.<br>'
+	# Remove the tarball
+	rm -f "$INSTA"
+
+	exit 0;
+}
+
+
+
+
+
+#########################################################################
+#
+# Main function begins here
+#
+#########################################################################
+
+#start_html
+
+
+# check if we were run without arguments, if so display "menu"
+case "$1" in
+	install)
+		install_harddisk;;
+esac
+
+
+cat <<EOF
+Welcome to the Torrent Watch Folder installer.<br>
+We are currently running from:<br>
+$urlpwd/$exename<br>
+<br>
+
+<a href="http://localhost.drives:8883/$urlpwd/$exename?install">
+Install to HARD_DISK
+</a>
+
+<br>
+
+
+EOF
+
+end_html
+
+exit 0
+
+
---
+++ tags/torrentwatch-dev-0.7.9/javascript/torrentwatch.js
@@ -0,0 +1,354 @@
+// Variable options based on chosen client
+function updateClientOptions() {
+	elem = document.getElementById('client');
+	if(!elem && document.parent)
+		elem = document.parent.getElementById('client');
+	if(!elem)
+		return;
+	changecss('div.favorite_seedratio', 'display', 'none');
+	switch(elem.value) {
+		case 'transmission1.3x':
+			showLayer('config_downloaddir');
+			showLayer('config_watchdir');
+			showLayer('config_savetorrent');
+			showLayer('config_deepdir');
+			showLayer('config_verifyepisodes');
+			changecss('div.favorite_seedratio', 'display', 'block');
+			changecss('div.favorite_savein', 'display', 'block');
+			changecss('div.favorite', 'height', '230');
+			changecss('div.favinfo', 'height', '230');
+			break;
+		case 'transmission1.22':
+			hideLayer('config_downloaddir');
+			showLayer('config_watchdir');
+			showLayer('config_savetorrent');
+			hideLayer('config_deepdir');
+			showLayer('config_verifyepisodes');
+			changecss('div.favorite_savein', 'display', 'none');
+			changecss('div.favorite', 'height', '180');
+			changecss('div.favinfo', 'height', '180');
+			break;
+		case 'btpd':
+			showLayer('config_downloaddir');
+			showLayer('config_watchdir');
+			showLayer('config_savetorrent');
+			showLayer('config_deepdir');
+			showLayer('config_verifyepisodes');
+			changecss('div.favorite_savein', 'display', 'block');
+			changecss('div.favorite', 'height', '205');
+			changecss('div.favinfo', 'height', '205');
+			break;
+		case 'nzbget':
+			hideLayer('config_downloaddir');
+			showLayer('config_watchdir');
+			hideLayer('config_savetorrent');
+			hideLayer('config_deepdir');
+			showLayer('config_verifyepisodes');
+			changecss('div.favorite_savein', 'display', 'none');
+			changecss('div.favorite', 'height', '180');
+			changecss('div.favinfo', 'height', '180');
+			break;
+		case 'sabnzbd':
+			hideLayer('config_downloaddir');
+			hideLayer('config_watchdir');
+			hideLayer('config_savetorrent');
+			hideLayer('config_deepdir');
+			showLayer('config_verifyepisodes');
+			changecss('div.favorite_savein', 'display', 'none');
+			changecss('div.favorite', 'height', '180');
+			changecss('div.favinfo', 'height', '180');
+			break;
+	}
+}
+
+// Buttons
+
+function saveConfig() {
+	toggleMenu('configuration');
+	showLayer('progressDiv');
+	setProgress('progressBar', 50);
+	setText('progressBar', 'Saving Config');
+	submitForm('config_form');
+}
+
+function saveWelcome() {
+	toggleMenu('welcome5');
+	showLayer('progressDiv');
+	setProgress('progressBar', 50);
+	setText('progressBar', 'Saving Config');
+	submitForm('welcome_form');
+}
+
+// Update Frame Utils
+function updateFrameLoad( url, progressText) {
+	setProgress('progressBar', 50);
+	setText('progressBar', progressText);
+	showLayer('progressDiv');
+	document.getElementById('update_frame').src = url;
+}
+
+function updateFrameCopyDiv( whichDiv ) {
+	parent.document.getElementById( whichDiv ).innerHTML = document.getElementById( whichDiv ).innerHTML;
+}
+
+function updateFrameFinished() {
+	parent.setText('progressBar', "Finished");
+	parent.hideLayer('progressDiv');
+}
+
+// Context Menu links 
+function contextAddToFav()
+{
+	location.assign(SimpleContextMenu._attachedElement.childNodes[0].href);
+}
+
+function contextDLNow()
+{
+	setProgress('progressBar', 20);
+	setText('progressBar', 'Starting Torrent');
+	showLayer('progressDiv');	
+	document.getElementById('update_frame').src = SimpleContextMenu._attachedElement.childNodes[1].href;
+}
+
+function contextInspect()
+{
+	showInspector(SimpleContextMenu._attachedElement.childNodes[2].textContent);
+}
+
+var $inspector_status = 0;
+
+function showInspector($title)
+{
+	$inspector_status = 1;
+	showLayer('inspector_container');
+	changecss('div#torrentlist_container', "right", "351");
+	changecss('div#filterbar_container', "right", "351");
+	if($title != '')
+		updateFrameLoad('/torrentwatch/inspector.cgi?title='+$title, 'Loading Inspector');
+}
+
+function hideInspector()
+{
+	$inspector_status = 0;
+	hideLayer('inspector_container');
+	changecss('div#torrentlist_container', "right", "0");
+	changecss('div#filterbar_container', "right", "0");
+}
+
+function toggleInspector()
+{
+	if($inspector_status)
+		hideInspector();
+	else
+		showInspector('');
+}
+
+function submitForm ( whichForm )
+{
+	document.getElementById(whichForm).submit();
+}
+
+// Function by Shawn Olsen
+function changecss(theClass,element,value) {
+	//Last Updated on May 21, 2008
+	//documentation for this script at
+	//http://www.shawnolson.net/a/503/altering-css-class-attributes-with-javascript.html
+	//
+	//Logic moved to getClassBySelector by Erik Bernhardson
+	var myClass = getClassBySelector(theClass);
+	if(myClass)
+		myClass.style[element] = value;
+}
+
+
+// Logic origionally in changecss by Shawn Olsen
+function getClassBySelector( whichClass ) {
+	var cssRules;
+	if (document.all) {
+		cssRules = 'rules';
+	} else if (document.getElementById) {
+		cssRules = 'cssRules';
+	}
+	for (var S = 0; S < document.styleSheets.length; S++) {
+		for (var R = 0; R < document.styleSheets[S][cssRules].length; R++) {
+			if(document.styleSheets[S][cssRules][R].selectorText == whichClass) {
+				return document.styleSheets[S][cssRules][R];
+			}
+		}
+	}
+}
+
+function changeFilter( filterType , data) {
+	changecss('UL.torrentlist LI.match_'+filterType, 'display', data); // IE
+	changecss('ul.torrentlist li.torrent.match_'+filterType, 'display', data); // FF
+}
+
+function markTorrentAlt()
+{
+	var alt = 0;
+	var elem = document.getElementById('torrentlist_container');
+	var textFilterActive = (document.getElementById('filter_text_input').value != '');
+	for( var F in elem.childNodes ) {
+		if ( elem.childNodes[F].className == 'feed' ) {
+			for ( var T in elem.childNodes[F].firstChild.childNodes ) {
+				var torrent = elem.childNodes[F].firstChild.childNodes[T];
+				if(torrent.className && torrent.className.substring(0,7) == 'torrent') {
+					var match_class = torrent.className.split(" ")[1];
+
+					var class_item = getClassBySelector('ul.torrentlist li.torrent.'+match_class);
+					if(!class_item)
+						class_item = getClassBySelector('UL.torrentlist LI.'+match_class);
+					if(!class_item)
+						return;
+
+					if(textFilterActive) {
+						if(torrent.style.display == "block") {
+							if(alt) {
+								torrent.className = "torrent "+match_class;
+								alt = 0;
+							} else {
+								torrent.className = "torrent "+match_class+" alt";
+								alt = 1;
+							}
+						}
+					} else {
+						if(class_item.style.display == "block") {
+							if(alt) {
+								torrent.className = "torrent "+match_class;
+								alt = 0;
+							} else {
+								torrent.className = "torrent "+match_class+" alt";
+								alt = 1;
+							}
+						}
+					}
+				}
+			}
+		}
+	}
+}
+
+function filterFeeds( filterType )
+{
+	var elem;
+	elem = document.getElementById('filter_'+filterType);
+	for ( var i in elem.parentNode.childNodes )
+	{
+		elem.parentNode.childNodes[i].className = ''
+	}
+	elem.className = 'selected';
+
+	switch(filterType) {
+		case 'all':
+			changeFilter('nomatch', 'block');
+			changeFilter('match', 'block');
+			changeFilter('cachehit', 'block');
+			changeFilter('duplicate', 'block');
+			changeFilter('old', 'block');
+			changeFilter('test', 'block');
+			break;
+		case 'matching':
+			changeFilter('nomatch', 'none');
+			changeFilter('match', 'block');
+			changeFilter('cachehit', 'block');
+			changeFilter('duplicate', 'block');
+			changeFilter('old', 'block');
+			changeFilter('test', 'block');
+			break;
+		case 'downloaded':
+			changeFilter('nomatch', 'none');
+			changeFilter('match', 'none');
+			changeFilter('cachehit', 'block');
+			changeFilter('duplicate', 'none');
+			changeFilter('old', 'none');
+			changeFilter('test', 'none');
+			break;
+	}
+	markTorrentAlt();
+}
+
+function filterFeedsByName() {
+	var filter_name = document.getElementById('filter_text_input').value;
+	var alt = 0;
+	var elem = document.getElementById('torrentlist_container');
+	for( var F in elem.childNodes ) {
+		if ( elem.childNodes[F].className == 'feed' ) {
+			for ( var T in elem.childNodes[F].firstChild.childNodes ) {
+				var torrent = elem.childNodes[F].firstChild.childNodes[T];
+				if(torrent.className && torrent.className.substring(0,7) == 'torrent') {
+					var torrent_name = torrent.childNodes[2].textContent;
+					if(filter_name == '')
+						torrent.style.display = '';
+					else if(torrent_name.toLowerCase().match(filter_name.toLowerCase()))
+						torrent.style.display = 'block';
+					else
+						torrent.style.display = 'none';
+				}
+			}
+		}
+	}
+	markTorrentAlt();
+}
+
+// Inspiration from http://www.netlobo.com/div_hiding.html
+
+var last_fav;
+function toggleFav( whichLayer )
+{
+	if(last_fav)
+		hideLayer(last_fav)
+	showLayer(whichLayer);
+	last_fav = whichLayer;
+	updateClientOptions();
+}
+
+var last_menu;
+function toggleMenu( whichLayer )
+{
+	if(last_menu && last_menu != whichLayer) {
+		hideLayer(last_menu);
+	}
+	toggleLayer(whichLayer);
+	last_menu = whichLayer;
+}
+
+function toggleLayer( whichLayer )
+{
+	var elem, vis;
+	if( document.getElementById ) // this is the way the standards work
+		elem = document.getElementById( whichLayer );
+	else if( document.all ) // this is the way old msie versions work
+			elem = document.all[whichLayer];
+	else if( document.layers ) // this is the way nn4 works
+		elem = document.layers[whichLayer];
+	vis = elem.style;
+	// if the style.display value is blank we try to figure it out here
+	if(vis.display==''&&elem.offsetWidth!=undefined&&elem.offsetHeight!=undefined)
+		vis.display = (elem.offsetWidth!=0&&elem.offsetHeight!=0)?'block':'none';
+	vis.display = (vis.display==''||vis.display=='block')?'none':'block';
+	if(whichLayer=='favorites') // Also display the first fav 
+		toggleFav(elem.childNodes[1].id);
+}
+
+function hideLayer( whichLayer ) {
+	var elem, vis;
+	if( document.getElementById ) // this is the way the standards work
+		elem = document.getElementById( whichLayer );
+	else if( document.all ) // this is the way old msie versions work
+			elem = document.all[whichLayer];
+	else if( document.layers ) // this is the way nn4 works
+		elem = document.layers[whichLayer];
+	if(elem)
+		elem.style.display = 'none';
+}
+function showLayer( whichLayer ) {
+	var elem, vis;
+	if( document.getElementById ) // this is the way the standards work
+		elem = document.getElementById( whichLayer );
+	else if( document.all ) // this is the way old msie versions work
+			elem = document.all[whichLayer];
+	else if( document.layers ) // this is the way nn4 works
+		elem = document.layers[whichLayer];
+	if(elem)
+		elem.style.display = 'block';
+}
