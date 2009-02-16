02e032d7bb0b09d728b8f87ef0f7c80d2d268828
somedude119 <somedude119@84144212-d151-0410-ac56-97821e5b5ab9>
2009-02-16 07:18:01 +0000

    break feed item html out of php/html.php

--- php/html.php
+++ php/html.php
@@ -3,13 +3,11 @@
 // browser, and in the NMT browser
 
 function setup_rss_list_html() {
-  global $html_out, $html_header;
-  $html_header = "<div class=feedlist>\n";
+  global $html_out;
   $html_out =  "<div id='torrentlist_container'>\n";
 }
 function finish_rss_list_html() {
-  global $html_out, $html_header;
-  $html_header .="</div>\n";
+  global $html_out;
   $html_out .=  "</div>\n";
 }
 
@@ -17,15 +15,18 @@ function show_torrent_html($item, $feed, $alt) {
   global $html_out, $matched, $test_run;
   // add word-breaking flags after each period
   $title = preg_replace('/\./', '.&shy;', $item['title']);
+  // prepare items for use in a url
+  $utitle = rawurlencode($item['title']);
+  $ulink = rawurlencode(get_torrent_link($item));
   $feed = urlencode($feed);
-  $html_out .= "<li class='torrent match_$matched $alt' title='"._isset($item, 'description')."'>";
-  $html_out .= "<a class='context_link' href='".$_SERVER['PHP_SELF']."/matchTitle?rss=$feed&title=".rawurlencode($item['title'])."'></a>";
-  $html_out .= "<a class='context_link' href='".$_SERVER['PHP_SELF']."/dlTorrent?title=".rawurlencode($item['title'])."&link=".rawurlencode(get_torrent_link($item))."'></a>";
-  $html_out .= "<span class='torrent_pubDate'>"._isset($item, 'pubDate').'</span>';
-  $html_out .= "<span class='torrent_name'>$title</span>";
-  $html_out .= "</li>\n";
+
+  ob_start();
+  require('templates/feed_item.tpl');
+  $html_out .= ob_get_contents();
+  ob_end_clean();
 }
 
+// The opening of the div which contains all the feeditems(one div per feed)
 function show_feed_html($rss, $idx) {
   global $html_out;
 
@@ -33,6 +34,7 @@ function show_feed_html($rss, $idx) {
   $html_out .= "<li class='header'>".$rss['title']."</li>\n";
 }
 
+// Closing the div which contains all the feed items
 function close_feed_html() {
   global $html_out;
   $html_out .= '</ul></div>';
---
+++ templates/feed_item.tpl
@@ -0,0 +1,10 @@
+<li class='torrent <?php echo "match_$matched $alt"; ?>' title='<?php echo _isset($item, 'description'); ?>'>
+<a class='context_link' 
+   href='<?php echo $_SERVER['PHP_SELF']; ?>/matchTitle?rss=$feed&title="<?php echo $utitle; ?>'>
+</a>
+<a class='context_link' 
+   href='<?php echo $_SERVER['PHP_SELF']; ?>/dlTorrent?title=<?echo $utitle; ?>&link=<?php echo $ulink; ?>'>
+</a>
+<span class='torrent_pubDate'><?php echo _isset($item, 'pubDate'); ?></span>
+<span class='torrent_name'><?php echo $title; ?></span>
+</li>
