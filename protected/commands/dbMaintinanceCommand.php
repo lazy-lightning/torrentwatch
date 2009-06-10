d83ba546b064eed36bee316d0c5e040ac7f42020
somedude119 <somedude119@84144212-d151-0410-ac56-97821e5b5ab9>
2009-06-10 00:21:40 +0000

    keep arround others,movies, and tvEpisodes of a downloaded status

--- protected/commands/dbMaintinanceCommand.php
+++ protected/commands/dbMaintinanceCommand.php
@@ -9,11 +9,15 @@ class dbMaintinanceCommand extends CConsoleCommand {
         'DELETE FROM feedItem'.
         ' WHERE feedItem.pubDate < '.(time()-3600*24*Yii::app()->dvrConfig->feedItemLifetime).';',
         // Delete others that dont point to a feed item anymore
+        // Unless they have been marked downloaded, so it doesn't download same title in future
         'DELETE FROM other'.
-        ' WHERE id NOT IN (SELECT other_id FROM feedItem);',
+        ' WHERE id NOT IN (SELECT other_id FROM feedItem)'.
+        '   AND status = '.other::STATUS_NEW.';',
         // Delete movies that dont point to a feed item anymore
+        // Unless they have been marked downloaded, so it doesn't download same title in future
         'DELETE FROM movie'.
-        ' WHERE id NOT IN (SELECT movie_id FROM feedItem);',
+        ' WHERE id NOT IN (SELECT movie_id FROM feedItem)'.
+        '   AND status = '.movie::STATUS_NEW.';',
         // Delete tvShows that dont point to a feed item(indirectly) or a favorite
         'DELETE FROM tvShow'.
         ' WHERE id NOT IN (SELECT tvShow_id FROM tvEpisode WHERE id IN (SELECT tvEpisode_id FROM feedItem))'.
@@ -22,8 +26,11 @@ class dbMaintinanceCommand extends CConsoleCommand {
         // note: due to the above sql, the tvShow should only be nonexistant when no feedItems as well
         //       but we can have a tvShow and no feedItems so maybee only select on not in feedItem ?
         'DELETE FROM tvEpisode'.
-        ' WHERE tvShow_id NOT IN (SELECT id FROM tvShow)'.
-        '    OR id NOT IN (SELECT tvEpisode_id FROM feedItem);',
+        ' WHERE ( tvShow_id NOT IN (SELECT id FROM tvShow)'.
+        '         OR'.
+        '         id NOT IN (SELECT tvEpisode_id FROM feedItem)'.
+        '       )'.
+        '   AND status = '.tvEpisode::STATUS_NEW.';',
         //
     );
     // SQLite doesn't do anything with foreign keys, so clean out MANY_MANY relationships that point to
@@ -55,6 +62,8 @@ class dbMaintinanceCommand extends CConsoleCommand {
          ),
 
     );
+    // could bind params instead with a custom createCommand(), but this is simple and straightforward, no chance of injection attack
+    // and its not run interactively so the little bit of extra speed is negligable
     foreach($pruneFk as $item)
       $querys[] = "DELETE FROM {$item['table']} WHERE {$item['fk']} NOT IN (SELECT id FROM {$item['pktable']});";
 
