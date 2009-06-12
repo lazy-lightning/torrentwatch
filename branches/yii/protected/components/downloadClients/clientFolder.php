95f664206c151cdf1f89f794260809b7fc2b08ac
somedude119 <somedude119@84144212-d151-0410-ac56-97821e5b5ab9>
2009-06-12 22:20:00 +0000

    fix wrong arguments to strtr

--- branches/yii/protected/components/downloadClients/clientFolder.php
+++ branches/yii/protected/components/downloadClients/clientFolder.php
@@ -9,7 +9,7 @@ class clientFolder extends BaseClient
   public function addByData($data) 
   {
     $saveIn = $this->getSaveInDirectory();
-    $title = strtr('/', '_', $this->manager->title);
+    $title = strtr($this->manager->title, '/', '_');
 
     $extension = $this->manager->downloadType == feedItem::TYPE_NZB ? 'nzb' : 'torrent';
 
@@ -23,6 +23,8 @@ class clientFolder extends BaseClient
       }
     }
 
+    Yii::log(print_r($this->manager->title, TRUE), CLogger::LEVEL_ERROR);
+    Yii::log("Writing $title to $filename", CLogger::LEVEL_ERROR);
     $return = file_put_contents($filename, $data);
     if(!$return)
         $this->_error = 'Unable to write to file: $filename';
