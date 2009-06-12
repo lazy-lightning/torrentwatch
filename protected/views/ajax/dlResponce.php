c39d27e7189f7023b7c37db7b53a3bde0e005d95
somedude119 <somedude119@84144212-d151-0410-ac56-97821e5b5ab9>
2009-06-12 21:58:24 +0000

    adjust fullResponce output to group items of same title, ordering by status needs more work to hide multiple related titles untill needed

--- protected/views/ajax/dlResponce.php
+++ protected/views/ajax/dlResponce.php
@@ -1,3 +1,9 @@
 <?php
+//  if(isset($error)) {
+//    echo '<h2>Download Error!</h2><pre>';
+    var_dump($error);
+//    echo '</pre>';
+//  }
+
   include 'protected/views/ajax/history_dialog.tpl';
 
