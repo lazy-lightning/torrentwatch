4884623637a16d27f65096c6f9cc2ba3714a5fa8
somedude119 <somedude119@84144212-d151-0410-ac56-97821e5b5ab9>
2009-06-14 23:39:39 +0000

    add missing files

---
+++ branches/yii/protected/components/feedAdapters/feedAdapter_File.php
@@ -0,0 +1,25 @@
+<?php
+
+require_once('SimplePie_1.1.3/simplepie.inc');
+
+class feedAdapter_File extends SimplePie_File {
+  function feedAdapter_File($url, $timeout = 10, $redirects = 5, $headers = null,
+                            $useragent = null, $force_fsockopen = false) {
+
+    // pretend for the sake of a particular private feed
+    if($useragent === null || substr($useragent, 0, 9) === 'SimplePie')
+      $useragent = 'UniversalFeedParser/4.01 +http://feedparser.org/';
+
+    // Translate :COOKIE: into http headers
+    if($cookies = stristr($url, ':COOKIE:')) {
+      $url = rtrim(substr($url, 0, -strlen($cookies)), "&");
+      $headers['Cookie'] = '$Version=1; '.strtr(substr($cookies, 8), '&', ';');
+    }
+    // convert &amp; into & because simplepie_file preserves it?
+    $url = str_replace('&amp;', '&', $url);
+
+    parent::SimplePie_File($url, $timeout, $redirects, $headers, $useragent, $force_fsockopen);
+    file_put_contents('/tmp/'.__FUNCTION__, serialize($this));
+  }
+}
+
