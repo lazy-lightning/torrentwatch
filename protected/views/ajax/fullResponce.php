3d64f7727b1061b58d352d1e231b201f0543145d
somedude119 <somedude119@84144212-d151-0410-ac56-97821e5b5ab9>
2009-06-15 23:47:52 +0000

    change all .tpl files into .php files

--- protected/views/ajax/fullResponce.php
+++ protected/views/ajax/fullResponce.php
@@ -2,12 +2,12 @@
   define('VIEWPATH','protected/views/ajax/');
   $logger = Yii::getLogger();
   Yii::log('start configuration_dialog: '.$logger->getExecutionTime(), CLogger::LEVEL_ERROR);
-  include VIEWPATH.'configuration_dialog.tpl';
+  include VIEWPATH.'configuration_dialog.php';
   Yii::log('start favorites_dialog: '.$logger->getExecutionTime(), CLogger::LEVEL_ERROR);
-  include VIEWPATH.'favorites_dialog.tpl';
-//  include VIEWPATH.'feeds_dialog.tpl';
+  include VIEWPATH.'favorites_dialog.php';
+//  include VIEWPATH.'feeds_dialog.php';
   Yii::log('start feedItems_dialog: '.$logger->getExecutionTime(), CLogger::LEVEL_ERROR);
-  include VIEWPATH.'feedItems_container.tpl';
+  include VIEWPATH.'feedItems_container.php';
   Yii::log('start history_dialog: '.$logger->getExecutionTime(), CLogger::LEVEL_ERROR);
-  include VIEWPATH.'history_dialog.tpl';
+  include VIEWPATH.'history_dialog.php';
 
