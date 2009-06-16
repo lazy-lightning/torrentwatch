918abe6ab3f5a81ba93b38346358b8b02f176ef9
somedude119 <somedude119@84144212-d151-0410-ac56-97821e5b5ab9>
2009-06-16 02:51:05 +0000

    command to scan others table for movies through imdb, not working yet

---
+++ protected/commands/scanOthersCommand.php
@@ -0,0 +1,44 @@
+<?php
+
+class scanOthersCommand extends CConsoleCommand {
+
+  public function run($args) {
+    $this->scanForMovies();
+  }
+
+  protected function scanForMovies() {
+    $db = Yii::app()->db;
+    $now = time();
+    $scanned = array();
+    $reader = $db->createCommand('SELECT id, title'.
+                                 '  FROM other'.
+                                 ' WHERE lastImdbUpdate = 0' // one shot
+
+    )->query();
+    foreach($reader as $row) {
+      $scanned[] = $row['id'];
+
+      $results = IMDbFetch::find($row['title']);
+      if(empty($results)) {
+        continue;
+      }
+
+      echo "Search for: ".$row['title']."\tResult: ".$results[0]['title']."\n";
+      continue;
+
+      // Delete other and recreate as movie
+      $movie = factory::movieByTitle($results[0]['title']);
+      if($movie->isNewRecord)
+        $movie->save();
+
+      $db->createCommand(
+          'UPDATE feedItem '.
+          '   SET movie_id='.$movie->id.', other_id=NULL'.
+          ' WHERE other_id='.$row['id']
+      )->execute();
+      other::model()->deleteByPk($row['id']);
+    }
+//    other::model()->updateByPk($scanned, array('lastImdbUpdate'=>$now));
+  }
+}
+
