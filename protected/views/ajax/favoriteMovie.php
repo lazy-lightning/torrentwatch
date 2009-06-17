6464c1cf12c68ec1b29bed56075d6b7f1eb725a4
somedude119 <somedude119@84144212-d151-0410-ac56-97821e5b5ab9>
2009-06-17 03:42:03 +0000

    attempt to clean out more of the improper items from a feed in feedAdapterNewzleech fix a $i i forgot to change in a previous checkin

--- protected/views/ajax/favoriteMovie.php
+++ protected/views/ajax/favoriteMovie.php
@@ -1,4 +1,4 @@
-<?php echo CHtml::beginForm(array('updateFavorite', 'id'=>$favorite->id), 'post', array('class'=>'favinfo', 'id'=>'favoriteMovies-'.$i++)); ?>
+<?php echo CHtml::beginForm(array('updateFavorite', 'id'=>$favorite->id), 'post', array('class'=>'favinfo', 'id'=>'favoriteMovies-'.$favorite->id)); ?>
  <div class="favorite_name">
   <?php 
     echo CHtml::activeLabel($favorite, 'name').': '.
