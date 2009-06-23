c89fe0aaf308f69990590fec8a7e37dba7029580
somedude119 <somedude119@84144212-d151-0410-ac56-97821e5b5ab9>
2009-06-23 07:53:11 +0000

    adjust usenetItems to cache their title since it gets called multiple times

--- protected/components/feedAdapters/usenetItem.php
+++ protected/components/feedAdapters/usenetItem.php
@@ -5,6 +5,8 @@
 // search for hdtv and worked ok
 class usenetItem extends feedAdapter_Item {
 
+  private $cleanTitle;
+
   private function clean($string, $cleaners) {
     foreach($cleaners as $reg => $pos) {
       if(preg_match($reg, $string, $regs)) {
@@ -15,24 +17,26 @@ class usenetItem extends feedAdapter_Item {
   }
 
   function get_title() {
-    $title = parent::get_title();
-    $cleaners = array(
-      '/&lt;(.*)&gt;/i' => 1,
-      '/#[\w\d.]+@[\w\d.]+[\] ]-[\[ ](?:req \d+ -)?([^\]]*)[\] ]?- ?\[?\d+\/\d+\]?/i' => 1,
-      '/presents (.*) \[\d+ of \d+\] &quot;.*&quot;/i' => 1,
-      '/\(([^):]+)\) \[\d+\/\d+\] - &quot;.*&quot;/i' => 1,
-      '/^([\w\d.]+(?:-\w+)?) ?&quot;.*&quot;/i' => 1,
-      '/^([A-Za-z0-9. ]+)&quot;.*&quot;/i' => 1,
-      '/&quot;(.*)&quot;/i' => 1,
-    );
-    $postClean = array(
-        '/^www.[\w\d-.]+ *(?:board request - )?(.*)$/i' => 1,
-    );
-    $newTitle = $this->clean($title, $cleaners);
-    $newTitle = $this->clean($newTitle, $postClean);
-    if($newTitle === $title)
-      Yii::log('Failed cleaning: '.$title, CLogger::LEVEL_ERROR);
-
-    return $newTitle;
+    if($this->cleanTitle === null) {  
+      $title = parent::get_title();
+      $cleaners = array(
+        '/&lt;(.*)&gt;/i' => 1,
+        '/#[\w\d.]+@[\w\d.]+[\] ]-[\[ ](?:req \d+ -)?([^\]]*)[\] ]?- ?\[?\d+\/\d+\]?/i' => 1,
+        '/presents (.*) \[\d+ of \d+\] &quot;.*&quot;/i' => 1,
+        '/\(([^):]+)\) \[\d+\/\d+\] - &quot;.*&quot;/i' => 1,
+        '/^([\w\d.]+(?:-\w+)?) ?&quot;.*&quot;/i' => 1,
+        '/^([A-Za-z0-9. ]+)&quot;.*&quot;/i' => 1,
+        '/&quot;(.*)&quot;/i' => 1,
+      );
+      $postClean = array(
+          '/^www.[\w\d-.]+ *(?:board request - )?(.*)$/i' => 1,
+      );
+      $newTitle = $this->clean($title, $cleaners);
+      $newTitle = $this->clean($newTitle, $postClean);
+      if($newTitle === $title)
+        Yii::log('Failed cleaning: '.$title, CLogger::LEVEL_ERROR);
+      $this->cleanTitle = $newTitle;
+    }
+    return $this->cleanTitle;
   }
 }
