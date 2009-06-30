273af543c478143a5f0e43a4db23e9a91364c116
somedude119 <somedude119@84144212-d151-0410-ac56-97821e5b5ab9>
2009-06-30 01:26:56 +0000

    update inspector views and change tabs to spaces

--- branches/yii/protected/views/ajax/inspectTvEpisode.php
+++ branches/yii/protected/views/ajax/inspectTvEpisode.php
@@ -1,33 +1,39 @@
 <?php $tvShow = $tvEpisode->tvShow; ?>
 <div>
-  <span>Title: </span>
-  <span><?php 
+  <h2 class='content'><?php 
     if(empty($tvShow->tvdbId))
       echo CHtml::encode($tvShow->title);
     else
       echo CHtml::link($tvShow->title, 'http://thetvdb.com/?tab=series&id='.$tvShow->tvdbId);
     ?>
-  </span>
+  </h2>
 </div>
-<div>
-  <span>Network: </span>
-  <span><?php echo CHtml::encode($tvShow->network->title); ?></span>
-</div>
-<div>
-  <span>Rating: </span>
-  <span><?php echo CHtml::encode($tvShow->rating); ?></span>
-</div>
-<div>
-  <span>Description: </span>
-  <span><?php echo CHtml::encode($tvShow->shortDescription); ?></span>
-</div>
+<?php if(!empty($tvShow->network_id)): ?>
+  <div>
+    <span>Network: </span>
+    <span class='content'><?php echo CHtml::encode($tvShow->network->title); ?></span>
+  </div>
+<?php endif; ?>
+<?php if(!empty($tvShow->rating)): ?>
+  <div>
+    <span>Rating: </span>
+    <span class='content'><?php echo CHtml::encode($tvShow->rating); ?></span>
+  </div>
+<?php endif; ?>
+<?php if(!empty($tvShow->description)): ?>
+  <div>
+    <span>Description: </span>
+    <span class='content'><?php echo CHtml::encode($tvShow->shortDescription); ?></span>
+  </div>
+<?php endif; ?>
 <br><br>
 <div>
-  <span><?php echo CHtml::encode($tvEpisode->episodeString); ?></span>
-  <span><?php echo CHtml::encode($tvEpisode->title); ?></span>
+  <h2><?php echo CHtml::encode($tvEpisode->episodeString); ?></h2>
+  <h2><?php echo CHtml::encode($tvEpisode->title); ?></h2>
 </div>
-<div>
-  <span>Description: </span>
-  <span><?php echo CHtml::encode($tvEpisode->description); ?></span>
-</div>
-   
+<?php if(!empty($tvEpisode->description)): ?>
+  <div>
+    <span>Description: </span>
+    <span class='content'><?php echo CHtml::encode($tvEpisode->description); ?></span>
+  </div>
+<?php endif; ?>
