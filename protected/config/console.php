d827e4661465b25238b2892755286af82100793d
somedude119 <somedude119@84144212-d151-0410-ac56-97821e5b5ab9>
2009-07-02 08:46:32 +0000

    adjust console to have seperate config not including CWebLogRoute

--- protected/config/console.php
+++ protected/config/console.php
@@ -1,1 +1,56 @@
-main.php
+<?php
+
+// uncomment the following to define a path alias
+// Yii::setPathOfAlias('local','path/to/local-folder');
+
+// This is the main Web application configuration. Any writable
+// CWebApplication properties can be configured here.
+return array(
+  'basePath'=>dirname(__FILE__).DIRECTORY_SEPARATOR.'..',
+  'name'=>'NMTDVR',
+
+  // preloading 'log' component
+  'preload'=>array('log'),
+
+  // autoloading model and component classes
+  'import'=>array(
+    'application.models.*',
+    'application.components.*',
+    'application.components.feedAdapters.*',
+    'application.components.downloadClients.*',
+  ),
+
+  // application components
+  'components'=>array(
+    'dlManager'=>array(
+      'class'=>'downloadManager',
+    ),
+    'dvrConfig'=>array(
+      'class'=>'dvrConfig',
+    ),
+    'log'=>array(
+      'class'=>'CLogRouter',
+      'routes'=>array(
+        array(
+          'class'=>'CFileLogRoute',
+          'levels'=>'info, error, warning, profile',
+        ),
+      ),
+    ),
+    'user'=>array(
+      // enable cookie-based authentication
+      'allowAutoLogin'=>true,
+    ),
+    'db'=>array(
+      'connectionString'=>'sqlite:protected/data/source.db',
+    ),
+  ),
+
+  // application-level parameters that can be accessed
+  // using Yii::app()->params['paramName']
+  'params'=>array(
+    // this is used in contact page
+    'adminEmail'=>'webmaster@example.com',
+    'dateFormat'=>'m-d-Y h:i a',
+  ),
+);
