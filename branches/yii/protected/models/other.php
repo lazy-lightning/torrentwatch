9964ce7f3e01a6c6c0eb7cb60803720a0250b71c
somedude119 <somedude119@84144212-d151-0410-ac56-97821e5b5ab9>
2009-07-02 09:19:48 +0000

    move default row defenitions from sqlite into yii, so its all visible in the same places

--- branches/yii/protected/models/other.php
+++ branches/yii/protected/models/other.php
@@ -30,6 +30,7 @@ class other extends CActiveRecord
   {
     return array(
         array('status', 'default', 'value'=>self::STATUS_NEW),
+        array('lastImdbUpdate', 'default', 'value'=>0),
     );
   }
 
