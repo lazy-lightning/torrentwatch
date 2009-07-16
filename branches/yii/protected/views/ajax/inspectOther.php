f4e766d4de9c52bbba16dc4a29ef6bf75ca10245
somedude119 <somedude119@84144212-d151-0410-ac56-97821e5b5ab9>
2009-07-16 03:20:23 +0000

    basic output for inspectOther

--- branches/yii/protected/views/ajax/inspectOther.php
+++ branches/yii/protected/views/ajax/inspectOther.php
@@ -1,3 +1,10 @@
-Inspect an other:
 
-<pre><?php var_dump($other->attributes); ?></pre>
+<div>
+  <h2><?php echo CHtml::encode($other->title); ?></h2>
+</div>
+<?php if(!empty($item->qualityString)): ?>
+  <div>
+    <span>Quality:</span>
+    <span class='content'><?php echo CHtml::encode($item->qualityString); ?></span>
+  </div>
+<?php endif;
