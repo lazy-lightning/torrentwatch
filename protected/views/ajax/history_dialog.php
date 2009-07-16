b2edcb9af28ddf4a37423f764930e69cd159e6eb
somedude119 <somedude119@84144212-d151-0410-ac56-97821e5b5ab9>
2009-07-16 01:13:46 +0000

    background images for all dialog windows adjusted dialog window markup to accomidate beautify configuration and wizard dialogs

--- protected/views/ajax/history_dialog.php
+++ protected/views/ajax/history_dialog.php
@@ -1,23 +1,25 @@
 <?php $h = False; ?>
 <div id="history" class="dialog_window">
- <ul>
-  <?php if(!empty($history)): ?>
-   <?php $n=0;foreach($history as $hItem): ?>
-    <li class='<?php echo ++$n%2?'alt':''; ?>'>
-      <div class="date"><?php echo date('Y M d h:i a', $hItem->date); ?></div>
-      <a href="#histItem<?php echo $hItem->id; ?>">
-          <?php echo $hItem->feedItem_title; ?>
-      </a>
-      <div class="hItemDetails" id="histItem<?php echo $hItem->id; ?>">
-        <div class="histFav"><?php echo $hItem->favorite_name; ?></div>
-        <div class="histFeed"><?php echo $hItem->feed_title; ?></div>
-      </div>
-    </li>
-   <?php endforeach; ?>
-  <?php endif; ?>
- </ul>
- <div class="buttonContainer">
-   <?php echo CHtml::link('Clear', array('clearHistory'), array('class'=>'historySubmit button', 'id'=>'clearHistory')); ?>
-   <a class="toggleDialog button" href="#">Close</a>
+  <div class="content">
+    <ul>
+    <?php if(!empty($history)): ?>
+    <?php $n=0;foreach($history as $hItem): ?>
+      <li class='<?php echo ++$n%2?'alt':''; ?>'>
+        <div class="date"><?php echo date('Y M d h:i a', $hItem->date); ?></div>
+        <a href="#histItem<?php echo $hItem->id; ?>">
+            <?php echo $hItem->feedItem_title; ?>
+        </a>
+        <div class="hItemDetails" id="histItem<?php echo $hItem->id; ?>">
+          <div class="histFav"><?php echo $hItem->favorite_name; ?></div>
+          <div class="histFeed"><?php echo $hItem->feed_title; ?></div>
+        </div>
+      </li>
+     <?php endforeach; ?>
+    <?php endif; ?>
+   </ul>
+  <div class="buttonContainer clearFix">
+     <?php echo CHtml::link('Clear', array('clearHistory'), array('class'=>'historySubmit button', 'id'=>'clearHistory')); ?>
+     <a class="toggleDialog button" href="#">Close</a>
+   </div>
  </div>
 </div>
--- protected/views/ajax/resetData.php
+++ protected/views/ajax/resetData.php
@@ -1,22 +1,24 @@
 <div class="dialog_window" id="reset_data"> 
-  <h2 class="dialog heading">Reset Stored Data</h2>
-  <ul>
-    <li class="clear">
-      <p>Delete all known data and update feeds from the internet</p>
-      <?php echo CHtml::link('All', array('resetData', 'type'=>'all'), array('class'=>'button ajaxSubmit')); ?>
-    </li>
-
-    <li class="clear">
-      <p>Reset all media types(episodes, movies, etc) to new status</p>
-      <?php echo CHtml::link('Media', array('resetData', 'type'=>'media'), array('class'=>'button ajaxSubmit')); ?>
-    </li>
-
-    <li class="clear">
-      <p>Reset all feed items to unmatched status</p>
-      <?php echo CHtml::link('Items', array('resetData', 'type'=>'feedItems'), array('class'=>'button ajaxSubmit')); ?>
-    </li>
-  </ul>
-  <div class="buttonContainer clear">
-    <a class="toggleDialog button" href="#">Close</a>
+  <div class="content">
+    <h2 class="dialog_heading">Reset Stored Data</h2>
+    <ul>
+      <li class="clearFix">
+        <?php echo CHtml::link('All', array('resetData', 'type'=>'all'), array('class'=>'button ajaxSubmit')); ?>
+        <p>Delete all known data and update feeds from the internet</p>
+      </li>
+  
+      <li class="clearFix">
+        <?php echo CHtml::link('Media', array('resetData', 'type'=>'media'), array('class'=>'button ajaxSubmit')); ?>
+        <p>Reset all media types(episodes, movies, etc) to new status</p>
+      </li>
+  
+      <li class="clearFix">
+        <?php echo CHtml::link('Items', array('resetData', 'type'=>'feedItems'), array('class'=>'button ajaxSubmit')); ?>
+        <p>Reset all feed items to unmatched status</p>
+      </li>
+    </ul>
+    <div class="buttonContainer clearFix">
+      <a class="toggleDialog button" href="#">Close</a>
+    </div>
   </div>
 </div> 
