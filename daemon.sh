98caa209022b6aa8ab102ccc565f317792814527
somedude119 <somedude119@84144212-d151-0410-ac56-97821e5b5ab9>
2009-07-18 07:08:35 +0000

    update daemon.sh to create directories excluded from the installation tarball

--- daemon.sh
+++ daemon.sh
@@ -6,4 +6,12 @@ if [ ! -f protected/data/source.db ]; then
   cp protected/data/source.db.orig protected/data/source.db
 fi
 
+if [ ! -d protected/runtime ]; then
+  mkdir protected/runtime
+fi
+
+if [ ! -d cache ]; then
+  mkdir cache
+fi
+
 chmod -R 777 .
