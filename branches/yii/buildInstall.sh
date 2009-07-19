1b21c6ffb5ff9a298b02b1bf77c0d3a4a854647f
somedude119 <somedude119@84144212-d151-0410-ac56-97821e5b5ab9>
2009-07-19 06:22:02 +0000

    minor updates to install builder

--- branches/yii/buildInstall.sh
+++ branches/yii/buildInstall.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
+# Build javascript.min
+cat $(cat index.html | grep \.js | grep -ve 'all|min' | sed 's,^.*javascript/\(.*\).js.*$,javascript/\1.js,') | java -jar testing/yuicompressor-2.4.2.jar --type js -o javascript/all.min.js
+
 # Used to build installation zip
 tar -cvf install/NMTDVR.tar . --exclude-vcs --exclude=install --exclude=testing --exclude=protected/data/source.db.BACKUP --exclude=protected/data/source.db --exclude=protected/runtime --exclude=cache
 cd install
