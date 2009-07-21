f8be019b09eb80d3bda3d68c6889395645b62090
somedude119 <somedude119@84144212-d151-0410-ac56-97821e5b5ab9>
2009-07-21 07:01:34 +0000

    break into multiple functions

--- protected/components/downloadClients/clientTransRPC.php
+++ protected/components/downloadClients/clientTransRPC.php
@@ -2,58 +2,102 @@
 
 class clientTransRPC extends BaseClient {
 
-  // browser emulator
-  private $be = null;
+  /**
+   * @var browserEmulator an instance configured for communicating with transmission
+   */
+  private $_be = null;
 
+  /**
+   * @param contents of a .torrent file to be added to transmission
+   * @return boolean if download was started
+   */
   function addByData($data) {
-    $dest = $this->getSaveInDirectory($data);
-    // transmission dies with bad folder if dest doesn't end in a /
-    if(substr($dest, -1, 1) !== '/')
-      $dest .= '/';
-    // transmission also doesn't like a doubled up / in the request uri
-    $api = $this->config->baseApi;
-    if(substr($api, -1, 1) !== '/')
-      $api .= '/';
-    $api .= 'rpc';
+    $be = $this->getBrowserEmulator();
 
-    if($this->be === null) {
-      $this->be = new browserEmulator();
+    $be->addPostData(json_encode(array(
+            'method'=>'torrent-add', 
+            'arguments'=>array(
+              'download-dir' => $this->getSaveInDirectory(),
+              'metainfo'     => base64_encode($data),
+            ),
+    )));
+
+    $response = $this->getTransmissionResponse($be);
+
+    return $this->checkResponse($response);
+  }
+
+  /**
+   * @param array responce array returned from transmission json request
+   * @return boolean if transmission successfully received the torrent
+   */
+  function checkResponse($response)
+  {
+    if(isset($response->result) AND ($response->result == 'success' or $response->result == 'duplicate torrent'))
+      return True;
+
+    if(isset($response->result))
+      $this->_error = "Transmission RPC Error: ".print_r($response);
+    else 
+      $this->_error = "Failure connecting to Transmission RPC at ".$this->getApi();
+
+    return False;
+  }
+
+  /**
+   * @return string URL for communicating with transmission via RPC
+   */
+  public function getApi()
+  {
+    // transmission doesn't like a doubled up / in the request uri
+    return rtrim($this->config->baseApi, '/').'/rpc';
+  }
+
+  /**
+   * @return browserEmulator object for communicating with transmission via RPC
+   */
+  function getBrowserEmulator()
+  {
+    if($this->_be === null) 
+    {
+      $this->_be = new browserEmulator();
       if(!empty($this->config->username))
-        $this->be->setAuth($this->config->username, $this->config->password);
-      $this->be->addHeaderLine('Content-Type', 'application/json');
-      $this->be->addHeaderLine('Connection', 'Close');
+        $this->_be->setAuth($this->config->username, $this->config->password);
+      $this->_be->addHeaderLine('Content-Type', 'application/json');
+      $this->_be->addHeaderLine('Connection', 'Close');
     }
+    $this->_be->resetPostData();
+    return $this->_be;
+  }
 
-    $this->be->resetPostData();
-    $args = array('download-dir' => $dest, 
-                  'metainfo'     => base64_encode($data));
-    $this->be->addPostData(json_encode(array('method'=>'torrent-add', 'arguments'=>$args)));
+  /**
+   * @return string directory to save download to
+   */
+  protected function getSaveInDirectory()
+  {
+    // transmission dies with bad folder if dest doesn't end in a /
+    return rtrim(parent::getSaveInDirectory(), '/').'/';
+  }
 
-
-    $response = $this->be->file_get_contents($api);
+  /**
+   * @param browserEmulator instance fully configured with post data to query transmission
+   * @return mixed response from transmission
+   */
+  function getTransmissionResponse($be)
+  {
+    $api = $this->getApi();
+    $response = $be->file_get_contents($api);
 
     // Invalid session id, set it and try again
     if(substr($response, 0, 7) === '<h1>409')
     {
       if(preg_match('/X-Transmission-Session-Id: ([A-Za-z0-9]+)/', $response, $regs)) {
-        $this->be->addHeaderLine('X-Transmission-Session-Id', $regs[1]);
-        $response = $this->be->file_get_contents($api);
+        $be->addHeaderLine('X-Transmission-Session-Id', $regs[1]);
+        $response = $be->file_get_contents($api);
       }
     }
         
-    $response = json_decode($response);
-
-    if(isset($response->result) AND ($response->result == 'success' or $response->result == 'duplicate torrent'))
-      return True;
-
-    file_put_contents('/tmp/transRpc.Failure', $this->be->lastRequest."\n\n".$this->be->lastResponse);
-
-    if(isset($response->result))
-      $this->_error = "Transmission RPC Error: ".print_r($response);
-    else 
-      $this->_error = "Failure connecting to Transmission RPC at ";
-
-    return False;
+    return json_decode($response);
   }
 
   public function getClassName() {
