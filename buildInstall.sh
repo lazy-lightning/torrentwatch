03ecf8aa4ffef1d1dd2961d450aaeb7b753cec7e
somedude119 <somedude119@84144212-d151-0410-ac56-97821e5b5ab9>
2009-07-21 02:17:08 +0000

    buildInstall now detects version from appinfo.json and names package accordingly

--- buildInstall.sh
+++ buildInstall.sh
@@ -1,10 +1,17 @@
 #!/bin/sh
 # Build javascript.min
+
+#VERSION
+VERSION=$(cat appinfo.json | grep version | sed 's/[ ]*.*="\(.*\)",/\1/g')
+
 cat $(cat index.html | grep \.js | grep -ve 'all|min' | sed 's,^.*javascript/\(.*\).js.*$,javascript/\1.js,') | java -jar testing/yuicompressor-2.4.2.jar --type js -o javascript/all.min.js
 
 # Used to build installation zip
 tar -cvf install/NMTDVR.tar . --exclude-vcs --exclude=install --exclude=testing --exclude=protected/data/source.db.BACKUP --exclude=protected/data/source.db --exclude=protected/runtime --exclude=cache
 cd install
-zip NMTDVR.zip * -x \*.zip
+zip NMTDVR-$VERSION.zip * -x \*.zip
+
+echo ''
+echo "Built install/NMTDVR-$VERSION.zip"
 
 
