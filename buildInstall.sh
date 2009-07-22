a9fb760e85bd304d09997deba271db57e0cbc5f9
somedude119 <somedude119@84144212-d151-0410-ac56-97821e5b5ab9>
2009-07-22 19:45:34 +0000

    change error reporting to E_ALL adjust buildInstall to also upload appinfo.json -> beta6

--- buildInstall.sh
+++ buildInstall.sh
@@ -7,11 +7,16 @@ VERSION=$(cat appinfo.json | grep version | sed 's/[ ]*.*="\(.*\)",/\1/g')
 cat $(cat index.html | grep \.js | grep -ve 'all|min' | sed 's,^.*javascript/\(.*\).js.*$,javascript/\1.js,') | java -jar testing/yuicompressor-2.4.2.jar --type js -o javascript/all.min.js
 
 # Used to build installation zip
-tar -cvf install/NMTDVR.tar . --exclude-vcs --exclude=install --exclude=testing --exclude=protected/data/source.db.BACKUP --exclude=protected/data/source.db --exclude=protected/runtime --exclude=cache
+tar -cvf install/NMTDVR.tar . --exclude-vcs --exclude=install --exclude=testing --exclude=protected/data/source.db.BACKUP --exclude=protected/data/source.db --exclude=protected/runtime --exclude=cache --exclude=buildInstall.sh --exclude=findNotSvn.sh
 cd install
 zip NMTDVR-$VERSION.zip * -x \*.zip
 
 echo ''
 echo "Built install/NMTDVR-$VERSION.zip"
 
+echo "Upload (Y/n)?"
+read CHAR
 
+if [ x"$CHAR" = x"Y" ];then
+  echo "cd public_html/downloads;put NMTDVR-$VERSION.zip" | lftp aso
+fi
