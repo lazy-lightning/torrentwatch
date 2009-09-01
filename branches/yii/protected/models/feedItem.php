58fde1869926de43fcc34dec228ab748a74ee9b8
somedude119 <somedude119@84144212-d151-0410-ac56-97821e5b5ab9>
2009-09-01 06:09:20 +0000

    new validation function notMultibyte prevents feeditems being created with multibyte strings in title/description adjust feedAdapter to gracefully skip feeditems that fail to save

--- branches/yii/protected/models/feedItem.php
+++ branches/yii/protected/models/feedItem.php
@@ -52,6 +52,7 @@ class feedItem extends ARwithQuality
       array('status', 'default', 'value'=>self::STATUS_NEW),
       array('status', 'in', 'allowEmpty'=>false, 'range'=>array_keys($this->getStatusOptions())),
       array('downloadType', 'in', 'allowEmpty'=>false, 'range'=>array_keys(feedItem::getStatusOptions())),
+      array('title,description', 'notMultibyte'),
     );
   }
 
@@ -212,4 +213,24 @@ class feedItem extends ARwithQuality
       return $this->other;
     return null;
   }
-}
+
+  /**
+   * Validator routine to ensure a string doesn't contain unsupported multibyte chars
+   * Function from http://us2.php.net/manual/en/function.mb-detect-encoding.php
+   * by chris AT w3style.co DOT uk based on code by php-note-2005 at ryandesign dot com
+   * @param string the string to check
+   */
+  function notMultibyte($string)
+  {
+    return false === preg_match('%(?:
+        [\xC2-\xDF][\x80-\xBF]             # non-overlong 2-byte
+        |\xE0[\xA0-\xBF][\x80-\xBF]        # excluding overlongs
+        |[\xE1-\xEC\xEE\xEF][\x80-\xBF]{2} # straight 3-byte
+        |\xED[\x80-\x9F][\x80-\xBF]        # excluding surrogates
+        |\xF0[\x90-\xBF][\x80-\xBF]{2}     # planes 1-3
+        |[\xF1-\xF3][\x80-\xBF]{3}         # planes 4-15
+        |\xF4[\x80-\x8F][\x80-\xBF]{2}     # plane 16
+        )+%xs', $string);
+  }
+
+} 
