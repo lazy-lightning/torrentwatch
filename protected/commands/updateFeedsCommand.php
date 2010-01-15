89c64be8783fa59bdf89701566b7576fdee7acb9
somedude119 <somedude119@84144212-d151-0410-ac56-97821e5b5ab9>
2010-01-15 07:48:06 +0000

    remove global transaction on updateFeeds command so as not to block while fetching feeds over the net.  individual transactions should be in place for all locations that write to the db.

--- protected/commands/updateFeedsCommand.php
+++ protected/commands/updateFeedsCommand.php
@@ -5,17 +5,10 @@ class updateFeedsCommand extends BaseConsoleCommand {
     $app = Yii::app();
     $feeds = feed::model()->findAll();
 
-    try {
-      $transaction = $app->db->beginTransaction();
-      foreach($feeds as $feed) {
-        echo "Updating {$feed->title} from {$feed->url}\n";
-        $feed->updateFeedItems(False); // false skips the check for favorites so we can check all at once
-      }
-      $app->dlManager->checkFavorites(feedItem::STATUS_NEW);
-      $transaction->commit();
-    } catch (Exception $e) {
-      $transaction->rollback();
-      throw $e;
+    foreach($feeds as $feed) {
+      echo "Updating {$feed->title} from {$feed->url}\n";
+      $feed->updateFeedItems(False); // false skips the check for favorites so we can check all at once
     }
+    $app->dlManager->checkFavorites(feedItem::STATUS_NEW);
   }
 }
