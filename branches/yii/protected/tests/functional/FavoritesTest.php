a106b1737cc70e0bce08cd6fb40a9ff04f5568a0
somedude119 <somedude119@84144212-d151-0410-ac56-97821e5b5ab9>
2010-02-01 09:07:20 +0000

    introduce per-test centralized locators array to make test code easier to read and update

--- branches/yii/protected/tests/functional/FavoritesTest.php
+++ branches/yii/protected/tests/functional/FavoritesTest.php
@@ -1,104 +1,135 @@
 <?php
 class FavoritesTest extends WebTestCase
 {
-  public $fixtures = array(
+  protected $fixtures = array(
       'favoriteTvShow'=>'favoriteTvShow',
       'favoriteMovie'=>'favoriteMovie',
       'favoriteString'=>'favoriteString',
       'tvEpisode'=>'tvEpisode',
   );
 
+  protected $locators = array(
+      'actionResponseDialog' => 'id=actionResponse',
+      'actionResponseHeading' => 'css=#actionResponse .dialog_heading',
+      'createFavoriteButton' => 'link=Create',
+      'deleteFavoriteButton' => 'link=Delete',
+      'errorSummary' => 'css=.errorSummary',
+      'favoriteFilterInput' => 'name={type}[filter]',
+      'favoriteMovieContainer' => 'id=favoriteMovie_container',
+      'favoriteNameInput' => "name={type}[{id}]",
+      'favoriteSaveInInput' => "css=#{type}-1 .favorite_savein input",
+      'favoriteStringContainer' => 'id=favoriteString_container',
+      'favoriteQualitySelect' => "{}div[@class='favorite_quality']/select",
+      'favoritesDialog' => 'id=favorites',
+      'favoriteForm' => '{}div',
+      'newFavoriteLink' => "xpath=id('{type}-li-')/a",
+      'saveFavoriteButton' => '{}div/a[1]',
+      'toggleFavoriteMovieTab' => "xpath=id('favorites')/div[2]/ul/li[2]/a",
+      'toggleFavoriteStringTab' => "xpath=id('favorites')/div[2]/ul/li[3]/a",
+      'toggleFavoritesButton' => 'link=Favorites',
+      'updateFavoriteButton' => 'link=Update',
+  );
+
   public function assertPreConditions()
   {
+    $l = $this->locators;
     parent::assertPreConditions();
-    $this->click('link=Favorites');
-    $this->waitForElementVisible('id=favorites');
+    $this->click($l['toggleFavoritesButton']);
+    $this->waitForElementVisible($l['favoritesDialog']);
+  }
+
+  // used for accessing different favorites without confusing the similar forms
+  protected function getLocators($type,$id) {
+    $xpath = "xpath=id('$type-1')/";
+    $out = array();
+    foreach($this->locators as $key =>$locator)
+        $out[$key] = str_replace(array('{}','{type}','{id}'), array($xpath,$type,$id), $locator);
+    return $out;
   }
 
   public function testTvShowFavorite()
   {
     // tvShow will be open be default
-    $this->realTest('favoriteTvShow', 'tvShow_id');
+    $this->realTest($this->getLocators('favoriteTvShow', 'tvShow_id'));
   }
 
   public function testMovieFavorite()
   {
+    $l = $this->getLocators('favoriteMovie', 'name');
     // Click the movies anchor
-    $this->click("xpath=id('favorites')/div[2]/ul/li[2]/a");
+    $this->click($l['toggleFavoriteMovieTab']);
     usleep(500000);
-    $this->waitForElementPresentAndVisible('id=favoriteMovie_container');
-    $this->realTest('favoriteMovie', 'name');
+    $this->waitForElementPresentAndVisible($l['favoriteMovieContainer']);
+    $this->realTest($l);
   }
 
   function testStringFavorite()
   {
+    $l = $this->getLocators('favoriteString', 'name');
     // Click the strings anchor
-    $this->click("xpath=id('favorites')/div[2]/ul/li[3]/a");
+    $this->click($l['toggleFavoriteStringTab']);
     usleep(500000);
-    $this->waitForElementPresentAndVisible('id=favoriteString_container');
-    $this->realTest('favoriteString', 'name');
+    $this->waitForElementPresentAndVisible($l['favoriteStringContainer']);
+    $this->realTest($l);
   }
 
-	public function realTest($type, $idAttr)
+	public function realTest($locators)
 	{
-    // shorthand for accessing first created favorite via xpath
-    $xpath = "xpath=id('$type-1')/";
-    // shorthand for accessing the ul of links
-    // Verify there is a link to make a new favorite
-    $this->assertElementPresent('link=New Favorite');
+    $l = $locators;
+    $this->assertElementPresent($l['newFavoriteLink']);
     // Click the new favorite link and wait for form to load
-    $this->click("xpath=id('$type-li-')/a");
-    $this->waitForElementPresentAndVisible('css=form.favinfo');
+    $this->click($l['newFavoriteLink']);
+    $this->waitForElementPresentAndVisible($l['createFavoriteButton']);
     // Enter a name and click create
-    $this->type("name={$type}[{$idAttr}]",'foobar');
+    $this->type($l['favoriteNameInput'],'foobar');
     // favoriteString also requires a filter
-    if($type === 'favoriteString') 
-      $this->type("name={$type}[filter]", 'bazzab');
-    $this->clickAndWaitFor('link=Create', "id=$type-1");
+    if($this->isElementPresent($l['favoriteFilterInput']))
+      $this->type($l['favoriteFilterInput'], 'bazzab');
+    $this->clickAndWaitFor($l['createFavoriteButton'], $l['updateFavoriteButton'],false);
     // Make sure our name got to this page
-    $this->assertElementNotPresent('css=div.errorSummary');
+    $this->assertElementNotPresent($l['errorSummary']);
     $this->assertTextPresent('foobar');
     // try a directory that cant be saved to
-    $this->type("css=#{$type}-1 .favorite_savein input", '/etc');
-    $this->click($xpath.'div/a[1]');
+    $this->type($l['favoriteSaveInInput'], '/etc');
+    $this->click($l['updateFavoriteButton']);
     // verify an error was presented
-    $this->waitForElementPresentAndVisible('css=div.errorSummary');
+    $this->waitForElementPresentAndVisible($l['errorSummary']);
     $this->assertTextPresent('glob:is not*writable*');
     // Try again with a directory that can save
-    $this->type("css=#{$type}-1 .favorite_savein input", '/tmp');
+    $this->type($l['favoriteSaveInInput'], '/tmp');
     // And change the qualitys arround
-    $this->select($xpath."div[@class='favorite_quality']/select[1]",'value=3');
-    $this->select($xpath."div[@class='favorite_quality']/select[2]",'value=5');
-    $this->click($xpath.'div/a[1]');
-    $this->waitForElementNotPresent('css=div.errorSummary');
+    $this->select($l['favoriteQualitySelect']."[1]",'value=3');
+    $this->select($l['favoriteQualitySelect']."[2]",'value=5');
+    $this->click($l['updateFavoriteButton']);
+    $this->waitForElementNotPresent($l['errorSummary']);
     // Verify our change saved
-    $this->assertValue($xpath."div/input[@name='{$type}[saveIn]']", '/tmp');
-    $this->assertValue($xpath."div[@class='favorite_quality']/select[1]", '3');
-    $this->assertValue($xpath."div[@class='favorite_quality']/select[2]", '5');
+    $this->assertValue($l['favoriteSaveInInput'], '/tmp');
+    $this->assertValue($l['favoriteQualitySelect']."[1]", '3');
+    $this->assertValue($l['favoriteQualitySelect']."[2]", '5');
     // Remove one of the qualitys
-    $this->select($xpath."div[@class='favorite_quality']/select[2]", 'value=-1');
+    $this->select($l['favoriteQualitySelect']."[2]", 'value=-1');
     // Click update button
-    $this->clickAndWaitFor($xpath.'div/a[1]');
+    $this->clickAndWaitFor($l['updateFavoriteButton']);
     // Verify it saved
-    $this->assertElementNotPresent('css=div.errorSummary');
-    $this->assertValue($xpath."div[@class='favorite_quality']/select[2]", '-1');
+    $this->assertElementNotPresent($l['errorSummary']);
+    $this->assertValue($l['favoriteQualitySelect']."[2]", '-1');
     // Delete it
-    $this->assertElementPresent('link=Delete');
-    $this->click('link=Delete');
+    $this->assertElementPresent($l['deleteFavoriteButton']);
+    $this->click($l['deleteFavoriteButton']);
     // Wait for the response dialog
-    $this->waitForElementPresentAndVisible('css=#actionResponse');
-    $this->waitForText('css=#actionResponse .dialog_heading', 'Delete Favorite');
+    $this->waitForElementPresentAndVisible($l['actionResponseDialog']);
+    $this->waitForText($l['actionResponseHeading'], 'Delete Favorite');
     // See if we got a successfull response
     $this->assertTextPresent('successful');
     // Make sure there isn't a link to view our deleted favorite still hanging arround
     $this->assertElementNotPresent('link=foobar');
     // Make sure the form was removed from the page
-    $this->assertElementNotPresent($xpath."div");
+    $this->assertElementNotPresent($l['favoriteForm']);
 	}
 
-  public function clickAndWaitFor($locator, $waitFor = 'id=favorites')
+  public function clickAndWaitFor($locator, $waitFor = 'id=favorites', $mid='id=progressbar')
   {
-    parent::clickAndWaitFor($locator, $waitFor);
+    parent::clickAndWaitFor($locator, $waitFor, $mid);
   }
 
 }
