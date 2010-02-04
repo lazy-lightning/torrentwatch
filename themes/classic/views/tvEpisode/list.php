1c3199334d5a4b5a948ff3558505a2e0cf3ba087
somedude119 <somedude119@84144212-d151-0410-ac56-97821e5b5ab9>
2010-02-04 04:40:07 +0000

    This view is called as part of the initial page load, as such simplify many of the convenience functions(like CHtml::link) to limit the cost of repeating 50+ times

--- themes/classic/views/tvEpisode/list.php
+++ themes/classic/views/tvEpisode/list.php
@@ -1,19 +1,22 @@
 <ul id="tv_container" class='loadContent'>
 <?php 
+// This is the first view loaded when opening the page, so most of the 
+// convenience methods were skipped to speed up rendering     
+$url = Yii::app()->getUrlManager();
 foreach($tvepisodeList as $n => $model) {
-  echo "<li id='tvEpisode-".$model->id."' class='torrent hasDuplicates match_".strtok($model->getStatusText(), ' ').($n%2?' alt':' notalt')."' >".
-       "<div class='itemButtons'>".
-         CHtml::link(CHtml::image('images/tor_start.png', 'Start', array('height'=>10)),
-             array('startDownload', 'id'=>$model->id), array('class'=>'startDownload ajaxSubmit')).
-         CHtml::link(CHtml::image('images/tor_fav.png', 'Favorite', array('height'=>10)),
-             array('makeFavorite', 'id'=>$model->id), array('class'=>'makeFavorite ajaxSubmit')).
-       "</div><div class='hideButton'>".
-         CHtml::link(CHtml::image('images/hide.png', 'Hide'),
-           array('hide', 'id'=>$model->id), array('class'=>'hideTvShow ajaxSubmit')).
-       "</div><div class='itemDetails'>".
-       "  <span class='name'>".CHtml::encode($model->tvShow->title)."</span>".
-       " - <span class='episode'>".CHtml::encode($model->getEpisodeString())."</span>".
-       (empty($model->title)?'':":  <span class='epTitle'>".CHtml::encode($model->title)."</span>").
+  echo "<li id='tvEpisode-".$model->id."' class='torrent hasDuplicates match_".strtok($model->asa('statusText')->getStatusText(), ' ').($n%2?' alt':' notalt')."' >".
+         "<div class='itemButtons'>".
+           "<a href='".$url->createUrl('tvEpisode/startDownload', array('id'=>$model->id))."' class='startDownload ajaxSubmit'>".
+           "<img src='images/tor_start.png' alt='Start' height='10'></a>".
+           "<a href='".$url->createUrl('tvEpisode/makeFavorite', array('id'=>$model->id))."' class='makeFavorite ajaxSubmit'>".
+           "<img src='images/tor_fav.png' alt='Favorite' height='10'></a>".
+         "</div><div class='hideButton'>".
+           "<a href='".$url->createUrl('tvEpisode/hide', array('id'=>$model->id))."' class='hideTvShow ajaxSubmit'>".
+           "<img src='images/hide.png' alt='Hide'></a>".
+         "</div><div class='itemDetails'>".
+         "  <span class='name'>".(empty($model->tvShow) ? $model->tvShow_id : CHtml::encode($model->tvShow->title))."</span>".
+         " - <span class='episode'>".CHtml::encode($model->getEpisodeString())."</span>".
+         (empty($model->title)?'':":  <span class='epTitle'>".CHtml::encode($model->title)."</span>").
        "</div></li>";
 } ?>
 <ul>
