8351a7f7d49f023de0b4737b3d0db773c733d0f8
somedude119 <somedude119@84144212-d151-0410-ac56-97821e5b5ab9>
2010-02-06 04:56:41 +0000

    Add some files that missed previous checkins - fixtures used in tvEpisode functional test - jquery 1.4.1

---
+++ branches/yii/protected/tests/fixtures/ItemTests/tvEpisode.php
@@ -0,0 +1,8 @@
+<?php
+
+return array(
+    array('tvShow_id'=>1,'season'=>1,'episode'=>1,'title'=>'Sample Tv Episode One'),
+    array('tvShow_id'=>1,'season'=>2,'episode'=>3,'title'=>'Sample Tv Episode Two'),
+    array('tvShow_id'=>1,'season'=>6,'episode'=>5,'title'=>'Sample Tv Episode Three'),
+);
+
---
+++ branches/yii/protected/tests/fixtures/history.php
@@ -0,0 +1,5 @@
+<?php
+return array(
+    0=>array('feedItem_id'=>0,'date'=>0,'feedItem_title'=>'Sample history', 'feed_title'=>'Sample Feed', 'favorite_name'=>'Sample Favorite'),
+    1=>array('feedItem_id'=>0,'date'=>0,'feedItem_title'=>'Sample history two'),
+);
