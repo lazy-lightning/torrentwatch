6e916cae19f9b3471351dc9b81b27fb43b8b532f
somedude119 <somedude119@84144212-d151-0410-ac56-97821e5b5ab9>
2010-02-15 23:47:43 +0000

    initial work to allow migration of database for old version of nmtdvr

---
+++ branches/yii/protected/commands/migrateCommand.php
@@ -0,0 +1,60 @@
+<?php
+
+class migrateCommand extends BaseConsoleCommand
+{
+  /**
+   * migrations 
+   * 
+   * @var array migration classes (version => class)
+   */
+  protected $migrations = array(
+  // db version => class to migrate to next version
+      0 => 'migrateFromNoVersion',
+      1 => 'migrateFromVersionOne',
+      2 => 'migrateFromVersionTwo',
+  );
+
+  // get version of database.  if version table doesnt exist create
+  // it and set version to 0
+  public function getDbVersion()
+  {
+    try {
+      $version = Yii::app()->db->createCommand(
+          'SELECT version FROM version'
+      )->queryScalar();
+    } catch (Exception $e) {
+      $cmds = array(
+          'CREATE TABLE version ( version INTEGER )',
+          'INSERT INTO version (version) VALUES (0)'
+      );
+      foreach($cmds as $sql)
+        Yii::app()->db->createCommand($sql)->execute();
+      $version = 1;
+    }
+    return $version;
+  }
+
+  public function run($args)
+  {
+    Yii::import('application.migrations.*');
+    $version = $this->getDbVersion();
+    echo "Current db version: $version\n";
+    while(isset($this->migrations[$version]))
+    {
+      $transaction = Yii::app()->db->beginTransaction();
+      try {
+        $class = $this->migrations[$version];
+        $migrate = new $class($this);
+        $migrate->run();
+        $version = $this->getDbVersion();
+        $transaction->commit();
+        echo "Migrated to version $version\n";
+      } catch (Exception $e) {
+        $transaction->rollback();
+        throw $e;
+      }
+    }
+  }
+}
+
+
