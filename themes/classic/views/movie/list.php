748028aa5fbf4e8db1198f426361788d1eaa813f
somedude119 <somedude119@84144212-d151-0410-ac56-97821e5b5ab9>
2010-02-15 04:04:24 +0000

    make movie and other list view like tvEpisode

--- themes/classic/views/movie/list.php
+++ themes/classic/views/movie/list.php
@@ -1,15 +1,15 @@
 <ul id="movie_container">
 <?php 
 foreach($movieList as $n => $model) {
-echo "<li id='movie-".$model->id."' class='torrent hasDuplicates match_".strtok($model->getStatusText(), ' ').($n%2?' alt':' notalt')."' >".
+  echo "<li id='movie-".$model->id."' class='torrent hasDuplicates match_".strtok($model->getStatusText(), ' ').($n%2?' alt':' notalt')."' >".
+     CHtml::link('', array('inspect', 'id'=>$model->id), array('class'=>'loadInspector ajaxSubmit', 'title'=>'Get More Information')).
      "<div class='itemButtons'>".
+       CHtml::link('Related FeedItems', array('/feedItem/list', 'related'=>'movie', 'id'=>$model->id),
+           array('class'=>'loadDuplicates')).
        CHtml::link(CHtml::image('images/tor_start.png', 'Start', array('height'=>10)),
-           array('startDownload', 'id'=>$model->id), array('class'=>'startDownload ajaxSubmit')).
+           array('startDownload', 'id'=>$model->id), array('class'=>'startDownload ajaxSubmit', title=>'Start Download')).
        CHtml::link(CHtml::image('images/tor_fav.png', 'Favorite', array('height'=>10)),
-           array('makeFavorite', 'id'=>$model->id), array('class'=>'makeFavorite ajaxSubmit')).
-     "</div><div class='hideButton'>".
-       CHtml::link(CHtml::image('images/hide.png', 'Hide'),
-         array('hideTvShow', 'id'=>$model->id), array('class'=>'hideTvShow ajaxSubmit')).
+           array('makeFavorite', 'id'=>$model->id), array('class'=>'makeFavorite ajaxSubmit', 'title'=>'Make Favorite')).
      "</div><div class='itemDetails'>".
      "  <span class='name'>".CHtml::encode($model->title)."</span>".
      "</div></li>";
