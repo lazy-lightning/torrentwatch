aa5d37cbbdffe7223961554089edceea78c0a9f3
somedude119 <somedude119@84144212-d151-0410-ac56-97821e5b5ab9>
2010-02-16 18:48:07 +0000

    update themes to nolonger point to images directly, but allow css

--- branches/yii/themes/classic/views/feedItem/list.php
+++ branches/yii/themes/classic/views/feedItem/list.php
@@ -4,10 +4,10 @@
       echo "<li class='torrent match_".strtok(feedItem::getStatusText($model->status), ' ').($n%2?' alt':' notalt')."' ".
            "    title='".CHtml::encode($model->description)."'>".
            "<div class='itemButtons'>".
-             CHtml::link(CHtml::image('images/tor_start.png', 'Start', array('height'=>10)), 
-                 array('startDownload', 'id'=>$model->id), array('class'=>'startDownload ajaxSubmit')).
-             CHtml::link(CHtml::image('images/tor_fav.png', 'Favorite', array('height'=>10)), 
-                 array('makeFavorite', 'id'=>$model->id), array('class'=>'makeFavorite ajaxSubmit')).
+             CHtml::link('&nbsp;', array('startDownload', 'id'=>$model->id), 
+                 array('class'=>'startDownload ajaxSubmit')).
+             CHtml::link('&nbsp;', array('makeFavorite', 'id'=>$model->id), 
+                 array('class'=>'makeFavorite ajaxSubmit')).
            "</div><div class='itemDetails'>".
            "  <span class='torrent_name'>".CHtml::encode($model->title)."</span>".
            "  <span class='torrent_feed'>".CHtml::encode($model->feed->title)."</span>".
--- branches/yii/themes/classic/views/tvEpisode/list.php
+++ branches/yii/themes/classic/views/tvEpisode/list.php
@@ -8,13 +8,10 @@ foreach($tvepisodeList as $n => $model) {
          "<a href='".$url->createUrl('tvEpisode/inspect', array('id'=>$model->id)),"' class='loadInspector ajaxSubmit' title='Get More Information'></a>".
          "<div class='itemButtons'>".
            "<a href='".$url->createUrl('feedItem/list', array('related'=>'tvEpisode', 'id'=>$model->id))."' class='loadDuplicates'>Related FeedItems</a>".
-           "<a href='".$url->createUrl('tvEpisode/startDownload', array('id'=>$model->id))."' class='startDownload ajaxSubmit'>".
-           "<img src='images/tor_start.png' alt='Start' height='10' width='10' title='Start Download'></a>".
-           "<a href='".$url->createUrl('tvEpisode/makeFavorite', array('id'=>$model->id))."' class='makeFavorite ajaxSubmit'>".
-           "<img src='images/tor_fav.png' alt='Favorite' height='10' width='10' title='Make Favorite'></a>".
+           "<a href='".$url->createUrl('tvEpisode/startDownload', array('id'=>$model->id))."' class='startDownload ajaxSubmit' title='Start Download'>&nbsp;</a>".
+           "<a href='".$url->createUrl('tvEpisode/makeFavorite', array('id'=>$model->id))."' class='makeFavorite ajaxSubmit' title='Make Favorite'>&nbsp;</a>".
          "</div><div class='hideButton'>".
-           "<a href='".$url->createUrl('tvShow/hide', array('id'=>$model->tvShow_id))."' class='hideTvShow ajaxSubmit'>".
-           "<img src='images/hide.png' alt='Hide' title='Hide Tv Show' height=8 width=8></a>".
+           "<a href='".$url->createUrl('tvShow/hide', array('id'=>$model->tvShow_id))."' class='hideTvShow ajaxSubmit'>&nbsp;</a>".
          "</div><div class='itemDetails'>".
          "  <span class='name'>".(empty($model->tvShow) ? $model->tvShow_id : CHtml::encode($model->tvShow->title))."</span>".
          " - <span class='episode'>".CHtml::encode($model->getEpisodeString())."</span>".
