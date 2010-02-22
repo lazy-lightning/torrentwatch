03e2a4830c4d3cd3b3cffa7a47aa093b884b2a93
somedude119 <somedude119@84144212-d151-0410-ac56-97821e5b5ab9>
2010-02-22 22:19:36 +0000

    couple more tests - verify elements in favorite forms line up properly without wrapping - verify only one form displayed when making 2 favorites in a row from tvEpisodeTest

--- protected/tests/fixtures/feed.php
+++ protected/tests/fixtures/feed.php
@@ -1,6 +1,6 @@
 <?php
 
 return array(
-    array('title'=>'All Feeds', 'description'=>'', 'url'=>'', 'status'=>0,'lastUpdated'=>0,'downloadType'=>0),
+    array('id'=>0, 'title'=>'All Feeds', 'description'=>'', 'url'=>'', 'status'=>0,'lastUpdated'=>0,'downloadType'=>0),
 );
 
