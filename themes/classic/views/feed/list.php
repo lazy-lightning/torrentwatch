0b578cd0f4243abe0ea67debea2c187cbd24eef5
somedude119 <somedude119@84144212-d151-0410-ac56-97821e5b5ab9>
2010-02-22 22:27:38 +0000

    changes needed to pass unit testing and functional testing in ff3.6

--- themes/classic/views/feed/list.php
+++ themes/classic/views/feed/list.php
@@ -1,7 +1,7 @@
 <div id="feeds">
   <h2 class="dialog_heading">Feeds</h2>
   <?php foreach($feedList as $n=>$model): if($model->id === '0') continue; // the generic 'all' feeds ?>
-    <?php if($model->id == 1) continue; ?>
+    <?php if($model->id == 0) continue; ?>
     <div class="activeFeed" title="<?php echo CHtml::encode($model->url); ?>">
       <?php echo CHtml::link('Delete', array('delete', 'id'=>$model->id), array('class'=>'button ajaxSubmit', 'id'=>'Delete')); ?>
       <span><?php echo CHtml::encode($model->title); ?></span>
