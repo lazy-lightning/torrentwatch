03864800cb624393449f8479090f16137008e0ac
somedude119 <somedude119@84144212-d151-0410-ac56-97821e5b5ab9>
2010-02-23 10:02:46 +0000

    couple more tests clean up some feedItem fixtures to point to feed 0 instead of 1

--- protected/tests/fixtures/favoriteManagerTvShowTest/feedItem.php
+++ protected/tests/fixtures/favoriteManagerTvShowTest/feedItem.php
@@ -1,7 +1,7 @@
 <?php
 
 return array(
-    'old'=>array('feed_id'=>1, 'tvEpisode_id'=>1,'status'=>1,'pubDate'=>0,    'lastUpdated'=>0,'title'=>'Sample Old Feed Item'),
-    'new'=>array('feed_id'=>1, 'tvEpisode_id'=>2,'status'=>1,'pubDate'=>30000,'lastUpdated'=>0,'title'=>'Sample New Feed Item'),
+    'old'=>array('feed_id'=>0, 'tvEpisode_id'=>1,'status'=>1,'pubDate'=>0,    'lastUpdated'=>0,'title'=>'Sample Old Feed Item'),
+    'new'=>array('feed_id'=>0, 'tvEpisode_id'=>2,'status'=>1,'pubDate'=>30000,'lastUpdated'=>0,'title'=>'Sample New Feed Item'),
 );
 
