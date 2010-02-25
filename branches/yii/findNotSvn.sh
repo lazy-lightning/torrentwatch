2b72f3822a2c2e538cd1cd4975794204077e371c
somedude119 <somedude119@84144212-d151-0410-ac56-97821e5b5ab9>
2010-02-25 04:33:27 +0000

    tweaks to build process

--- branches/yii/findNotSvn.sh
+++ branches/yii/findNotSvn.sh
@@ -1,1 +1,1 @@
-svn status | grep -e ^? | egrep -v 'tmp|cache/|protected/runtime|protected/data/source.db|assets/|install/NMTDVR'
+svn status | grep -e ^? | egrep -v 'tmp|cache/|protected/runtime|protected/data/source.db|assets/|install/NMTDVR|source-test.db.BACKUP|PHPUnit|phpunit'
