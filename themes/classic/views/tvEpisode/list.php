d26d9f4e6a3a3c3119b70a2a08abef04fd05cdf2
somedude119 <somedude119@84144212-d151-0410-ac56-97821e5b5ab9>
2010-02-27 06:16:44 +0000

    optimizations after profiling

--- themes/classic/views/tvEpisode/list.php
+++ themes/classic/views/tvEpisode/list.php
@@ -1,10 +1,15 @@
 <ul id="tv_container" class='loadContent'>
-<?php 
+<?php
+function fakenew($class)
+{
+  // from PHPUnit_Framework_TestCase::getMock()
+  return unserialize(sprintf('O:%d:"%s":0:{}',strlen($class), $class));
+}
 // This is the first view loaded when opening the page, so most of the 
 // convenience methods were skipped to speed up rendering     
 $url = Yii::app()->getUrlManager();
-$feedItem = feedItem::model();
-$tvEpisode = tvEpisode::model();
+$feedItem = fakenew('feedItem');
+$tvEpisode = fakenew('tvEpisode');
 $inspect = $url->createUrl('tvEpisode/inspect', array('id'=>'{id}'));
 $list = $url->createUrl('feedItem/list', array('related'=>'tvEpisode', 'id'=>'{id}'));
 $start = $url->createUrl('tvEpisode/startDownload', array('id'=>'{id}'));
@@ -14,20 +19,32 @@ foreach($tvepisodeList as $n => $row) {
   $id = $row['id'];
   $epString = CHtml::encode($tvEpisode->getEpisodeString($row['season'], $row['episode']));
   $status = strtok($feedItem->getStatusText($row['feedItem_status']), ' ');
-  echo "<li id='tvEpisode-$id' class='torrent hasDuplicates match_$status".($n%2?' alt':' notalt')."' >".
-         "<a href='".str_replace('%7Bid%7D', $id, $inspect)."' class='loadInspector ajaxSubmit' title='Get Detailed Media Information'></a>".
-         "<div class='itemButtons'>".
-           "<a href='".str_replace('%7Bid%7D', $id, $list)."' class='loadDuplicates'>Related FeedItems</a>".
-           "<a href='".str_replace('%7Bid%7D', $id, $start)."' class='startDownload ajaxSubmit' title='Start Download'>&nbsp;</a>".
-           "<a href='".str_replace('%7Bid%7D', $id, $makeFav)."' class='makeFavorite ajaxSubmit' title='Make Favorite'>&nbsp;</a>".
-         "</div><div class='hideButton'>".
-           "<a href='".str_replace('%7Btid%7D', $row['tvShow_id'], $hide)."' class='hideTvShow ajaxSubmit' title='Hide from listing'>&nbsp;</a>".
-         "</div><div class='itemDetails'>".
-         "  <span class='name'>".CHtml::encode($row['tvShow_title'])."</span>".
-         "  <span class='episode'>$epString</span>".
-         (empty($row['title'])?'':(":  <span class='epTitle'>".CHtml::encode($row['title'])."</span>")).
-         "  <span class='torrent_pubDate'>".CHtml::encode(date("M d h:i a", $row['lastUpdated']))."</span>".
-       "</div></li>";
+  $alt = $n%2?' alt':' notalt';
+  $title = CHtml::encode($row['tvShow_title']);
+  $epTitle = empty($row['title'])?'':(":  <span class='epTitle'>".CHtml::encode($row['title'])."</span>");
+  $pubDate = date("M d h:i a", $row['lastUpdated']);
+  $l = str_replace('%7Bid%7D', $id, $list);
+  $s = str_replace('%7Bid%7D', $id, $start);
+  $m = str_replace('%7Bid%7D', $id, $makeFav);
+  $h = str_replace('%7Btid%7D', $row['tvShow_id'], $hide);
+  echo <<<EOD
+<li id='tvEpisode-$id' class='torrent hasDuplicates match_$status $alt'>
+  <a href='".str_replace('%7Bid%7D', $id, $inspect)."' class='loadInspector ajaxSubmit' title='Get Detailed Media Information'></a>
+  <div class='itemButtons'>
+    <a href='$l' class='loadDuplicates'>Related FeedItems</a>
+    <a href='$s' class='startDownload ajaxSubmit' title='Start Download'>&nbsp;</a>
+    <a href='$m' class='makeFavorite ajaxSubmit' title='Make Favorite'>&nbsp;</a>
+  </div><div class='hideButton'>
+    <a href='$h' class='hideTvShow ajaxSubmit' title='Hide from listing'>&nbsp;</a>
+  </div><div class='itemDetails'>
+    <span class='name'>$title</span>
+    <span class='episode'>$epString</span>
+    $epTitle
+    <span class='torrent_pubDate'>$pubDate</span>
+  </div>
+</li>
+EOD
+  ;
 } ?>
 </ul>
  
