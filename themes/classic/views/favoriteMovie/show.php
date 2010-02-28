33d03d9e9f707986b8f3929f3e4438713c601001
somedude119 <somedude119@84144212-d151-0410-ac56-97821e5b5ab9>
2010-02-28 09:58:55 +0000

    use grayed out text in input boxes for all favorites

--- themes/classic/views/favoriteMovie/show.php
+++ themes/classic/views/favoriteMovie/show.php
@@ -1,67 +1,67 @@
 <?php 
-  echo CHtml::beginForm(
+  echo html::beginForm(
       array('/favoriteMovie/'.($model->isNewRecord ? 'create' : 'update'), 'id'=>$model->id), 
       'post', 
       array('class'=>'favinfo', 'id'=>'favoriteMovie-'.$model->id)
   );
-  echo CHtml::errorSummary($model);
+  echo html::errorSummary($model);
   if(isset($success) && $success): ?>
     <div class='saved'>Saved</div>
 <?php endif; ?>
  <div class="favorite_name">
   <?php 
-    echo CHtml::activeLabelEx($model, 'name', array('title'=>'Must be unique and does not effect matching')).': '.
-         CHtml::activeTextField($model, 'name', array('title'=>'Must be unique and does not effect matching')); ?>
+    echo html::activeLabelEx($model, 'name', array('title'=>'Must be unique and does not effect matching')).': '.
+         html::activeTextField($model, 'name', array('title'=>'Must be unique and does not effect matching', 'gray'=>'Unique title not effecting matching')); ?>
  </div>
  <div class="favorite_savein">
-  <?php echo CHtml::activeLabelEx($model, 'saveIn', array('title'=>'A valid writable directory to pass to the download clients')).': '.
-             CHtml::activeTextField($model, 'saveIn', array('title'=>'A valid writable directory to pass to the download clients')); ?>
+  <?php echo html::activeLabelEx($model, 'saveIn', array('title'=>'A valid writable directory to pass to the download clients')).': '.
+             html::activeTextField($model, 'saveIn', array('title'=>'A valid writable directory to pass to the download clients', 'gray'=>'Valid writable directory')); ?>
  </div>
  <div class="favorite_feed">
-  <?php echo CHtml::activeLabelEx($model, 'feed_id').': '.
-             CHtml::dropDownList('favoriteMovie[feed_id]', $model->feed_id, $feedsListData); ?>
+  <?php echo html::activeLabelEx($model, 'feed_id').': '.
+             html::dropDownList('favoriteMovie[feed_id]', $model->feed_id, $feedsListData); ?>
  </div>
  <div class="favorite_rating">
-   <?php echo CHtml::activeLabelEx($model, 'rating', array('title'=>'This minimum rating out of 100 to match')).': '.
-              CHtml::activeTextField($model, 'rating', array('title'=>'The minimum rating out of 100 to match')); ?>
+   <?php echo html::activeLabelEx($model, 'rating', array('title'=>'This minimum rating out of 100 to match')).': '.
+              html::activeTextField($model, 'rating', array('title'=>'The minimum rating out of 100 to match', 'gray'=>'Minimum out of 100')); ?>
  </div>
  <div class="favorite_years">
-   <?php echo CHtml::activeLabel($model, 'year', array('title'=>'Movie must be between these years to be matched')).': <span>'.
-              CHtml::activeTextField($model, 'minYear', array('title'=>'The minimum movie year to match')).'-'.
-              CHtml::activeTextField($model, 'maxYear', array('title'=>'The maximum movie year to match')).'</span>'; ?>
+   <?php echo html::activeLabel($model, 'year', array('title'=>'Movie must be between these years to be matched')).': <span>'.
+              html::activeTextField($model, 'minYear', array('title'=>'The minimum movie year to match', 'gray'=>'min')).'-'.
+              html::activeTextField($model, 'maxYear', array('title'=>'The maximum movie year to match', 'gray'=>'max')).'</span>'; ?>
  </div>
  <div class="favorite_genre">
-  <?php echo CHtml::activeLabelEx($model, 'genre_id').': '.
-             CHtml::dropDownList('favoriteMovie[genre_id]', $model->genre_id, $genresListData); ?>
+  <?php echo html::activeLabelEx($model, 'genre_id').': '.
+             html::dropDownList('favoriteMovie[genre_id]', $model->genre_id, $genresListData); ?>
  </div>
  <div class="favorite_quality">
   <?php  // show min 3 qualitys always, even if less are set
-    echo CHtml::activeLabelEx($model, 'quality').': ';
+    echo html::activeLabelEx($model, 'quality').': ';
     $qualitysListData = quality::getCHtmlListData();
     $j=0;foreach($model->quality as $quality) {
-      echo CHtml::dropDownList('quality_id['.++$j.']', $quality->id, $qualitysListData);
+      echo html::dropDownList('quality_id['.++$j.']', $quality->id, $qualitysListData);
     }
     for(++$j;$j<4;++$j)
-      echo CHtml::dropDownList('quality_id['.$j.']', -1, $qualitysListData);
+      echo html::dropDownList('quality_id['.$j.']', -1, $qualitysListData);
   ?>
  </div>
  <div class="favorite_queue">
-   <?php echo CHtml::activeLabelEx($model, 'queue', array('title'=>'Do not automatically download. Queue for user input')).': '.
-              CHtml::activeCheckBox($model, 'queue', array('title'=>'Do not automatically download. Queue for user input')); ?>
+   <?php echo html::activeLabelEx($model, 'queue', array('title'=>'Do not automatically download. Queue for user input')).': '.
+              html::activeCheckBox($model, 'queue', array('title'=>'Do not automatically download. Queue for user input')); ?>
  </div>
  <div class="buttonContainer">
    <a class="submitForm button" class="update" href="#"><?php echo ($model->isNewRecord ? 'Create' : 'Update'); ?></a>
    <?php if(!$model->isNewRecord)
-           echo CHtml::link('Delete', array('delete', 'id'=>$model->id), array('class'=>'button ajaxSubmit')); ?>
+           echo html::link('Delete', array('delete', 'id'=>$model->id), array('class'=>'button ajaxSubmit')); ?>
    <a class="toggleDialog button" href="#">Close</a>
  </div>
 <?php 
 if(isset($success,$create) && $success && $create)
 {
-  echo "<li id='favoriteMovie-li-{$model->id}'>".CHtml::link(
+  echo "<li id='favoriteMovie-li-{$model->id}'>".html::link(
     $model->name,
     array('/favoriteMovie/show', 'id'=>$model->id),
     array('rel'=>'#favoriteMovie-'.$model->id)
   )."</li>"; 
 }
-echo CHtml::endForm().(isset($response) ? $response : '');
+echo html::endForm().(isset($response) ? $response : '');
--- themes/classic/views/favoriteString/show.php
+++ themes/classic/views/favoriteString/show.php
@@ -1,63 +1,63 @@
 <?php 
-  echo CHtml::beginForm(
+  echo html::beginForm(
       array('/favoriteString/'.($model->isNewRecord ? 'create' : 'update'), 'id'=>$model->id), 
       'post', 
       array('class'=>'favinfo', 'id'=>'favoriteString-'.$model->id)
   );
-  echo CHtml::errorSummary($model);
+  echo html::errorSummary($model);
   if(isset($success) && $success): ?>
     <div class='saved'>Saved</div>
 <?php endif; ?>
 
  <div class="favorite_name">
   <?php 
-    echo CHtml::activeLabelEx($model, 'name', array('title'=>'Must be unique and does not effect matching')).': '.
-         CHtml::activeTextField($model, 'name', array('title'=>'Must be unique and does not effect matching')); ?>
+    echo html::activeLabelEx($model, 'name', array('title'=>'Must be unique and does not effect matching')).': '.
+         html::activeTextField($model, 'name', array('title'=>'Must be unique and does not effect matching', 'gray'=>'Unique title not effecting matching')); ?>
  </div>
  <div class="favorite_savein">
-  <?php echo CHtml::activeLabelEx($model, 'saveIn', array('title'=>'A valid writable directory to pass to the download clients')).': '.
-             CHtml::activeTextField($model, 'saveIn', array('title'=>'A valid writable directory to pass to the download clients')); ?>
+  <?php echo html::activeLabelEx($model, 'saveIn', array('title'=>'A valid writable directory to pass to the download clients')).': '.
+             html::activeTextField($model, 'saveIn', array('title'=>'A valid writable directory to pass to the download clients', 'gray'=>'Valid writable directory')); ?>
  </div>
  <div class="favorite_feedId">
-  <?php echo CHtml::activeLabelEx($model, 'feedId').': '.
-             CHtml::dropDownList('favoriteString[feed_id]', $model->feed_id, feed::getCHtmlListData()); ?>
+  <?php echo html::activeLabelEx($model, 'feedId').': '.
+             html::dropDownList('favoriteString[feed_id]', $model->feed_id, feed::getCHtmlListData()); ?>
  </div>
  <div class="favorite_filter">
-   <?php echo CHtml::activeLabelEx($model, 'filter', array('title'=>'Feed Item title must match this string to be downloaded.  * is accepted')).': '.
-              CHtml::activeTextField($model, 'filter', array('title'=>'Feed Item title must match this string to be downloaded.  * is accepted')); ?>
+   <?php echo html::activeLabelEx($model, 'filter', array('title'=>'Feed Item title must match this string to be downloaded.  * is accepted')).': '.
+              html::activeTextField($model, 'filter', array('title'=>'Feed Item title must match this string to be downloaded.  * is accepted', 'gray'=>'You can use *')); ?>
  </div>
  <div class="favorite_notFilter">
-   <?php echo CHtml::activeLabelEx($model, 'notFilter', array('title'=>'Any feed item title matching this filter will be excluded')).': '.
-              CHtml::activeTextField($model, 'notFilter', array('title'=>'Any feed item matching this filter will be excluded')); ?>
+   <?php echo html::activeLabelEx($model, 'notFilter', array('title'=>'Any feed item title matching this filter will be excluded')).': '.
+              html::activeTextField($model, 'notFilter', array('title'=>'Any feed item matching this filter will be excluded', 'gray'=>'Feeditem title must not match')); ?>
  </div>
  <div class="favorite_quality">
   <?php  // show min 3 qualitys always, even if less are set
-    echo CHtml::activeLabelEx($model, 'quality').': ';
+    echo html::activeLabelEx($model, 'quality').': ';
     $qualitysListData = quality::getCHtmlListData();
     $j=0;foreach($model->quality as $quality) {
-      echo CHtml::dropDownList('quality_id['.++$j.']', $quality->id, $qualitysListData);
+      echo html::dropDownList('quality_id['.++$j.']', $quality->id, $qualitysListData);
     }
     for(++$j;$j<4;++$j)
-      echo CHtml::dropDownList('quality_id['.$j.']', -1, $qualitysListData);
+      echo html::dropDownList('quality_id['.$j.']', -1, $qualitysListData);
   ?>
  </div>
  <div class="favorite_queue">
-   <?php echo CHtml::activeLabelEx($model, 'queue', array('title'=>'Do not automatically download.  Queue for user input')).': '.
-              CHtml::activeCheckBox($model, 'queue', array('title'=>'Do not automatically download.  Queue for user input')); ?>
+   <?php echo html::activeLabelEx($model, 'queue', array('title'=>'Do not automatically download.  Queue for user input')).': '.
+              html::activeCheckBox($model, 'queue', array('title'=>'Do not automatically download.  Queue for user input')); ?>
  </div>
  <div class="buttonContainer">
    <a class="submitForm button" id="Update" href="#"><?php echo $model->isNewRecord ? 'Create' : 'Update'; ?></a>
    <?php if(!$model->isNewRecord)
-           echo CHtml::link('Delete', array('delete', 'id'=>$model->id), array('class'=>'button ajaxSubmit')); ?>
+           echo html::link('Delete', array('delete', 'id'=>$model->id), array('class'=>'button ajaxSubmit')); ?>
    <a class="toggleDialog button" href="#">Close</a>
  </div>
 <?php 
 if(isset($success,$create) && $success && $create)
 {
-  echo "<li id='favoriteString-li-{$model->id}'>".CHtml::link(
+  echo "<li id='favoriteString-li-{$model->id}'>".html::link(
     $model->name,
     array('/favoriteString/show', 'id'=>$model->id),
     array('rel'=>'#favoriteString-'.$model->id)
   )."</li>";
 }
-echo CHtml::endForm().(isset($response) ? $response : '');
+echo html::endForm().(isset($response) ? $response : '');
