cfb96922e6d5d1bdece22ff468e0e184d56aeb7a
somedude119 <somedude119@84144212-d151-0410-ac56-97821e5b5ab9>
2010-03-01 02:17:44 +0000

    minor updates to buildInstall clean up a few css rules

--- branches/yii/buildInstall.sh
+++ branches/yii/buildInstall.sh
@@ -16,6 +16,21 @@ EXCLUDES="install testing protected/data/source-test.db.BACKUP protected/data/so
 IS_SVN_RELEASE=1
 CURRENTSVN=$(svn status | egrep -v '^\?|buildInstall.sh|source-test.db')
 
+if [ x"$1" = x"--release" -o x"$1" = x"-r" ]; then
+  # dont generate an svn version
+  IS_SVN_RELEASE=0
+  # release mode, allow changes to appinfo
+  CURRENTSVN=$(echo $CURRENTSVN | grep -v appinfo.json)
+elif [ x"$1" != x"" ]; then
+  cat <<EOD
+
+  Usage:
+    $0 [-r|--release]
+
+EOD
+  exit;
+fi
+
 # test php for basic syntax errors
 find ./ -iname \*.php | egrep -v 'svn|yii_framework|PHPUnit' | xargs -L 1 php -l 
 if [ ! 0 -eq $? ];then
@@ -24,13 +39,6 @@ if [ ! 0 -eq $? ];then
 Please fix lint errors before building package.
 EOD
   exit 1;
-fi
-
-if [ x"$1" = x"release" ]; then
-  # dont generate an svn version
-  IS_SVN_RELEASE=0
-  # release mode, allow changes to appinfo
-  CURRENTSVN=$(echo $CURRENTSVN | grep -v appinfo.json)
 fi
 
 if [ x"$CURRENTSVN" != x"" -o x"SKIPSVN" = x"1" ];then
@@ -42,11 +50,12 @@ EOD
   exit 1
 fi
 
+# Generate a new versioo number for svn releases
 if [ $IS_SVN_RELEASE -eq 1 ]; then
   # start with date
   VERSION="svn$(date +%Y%m%d)"
   I=0
-  # if that version already exists, increment i untill it doesnt exist
+  # if that version already exists, increment I untill it doesnt exist
   while [ -f install/$NAME-$VERSION.zip ]; do
     I=$(expr $I + 1)
     VERSION="svn$(date +%Y%m%d).${I}"
@@ -60,17 +69,21 @@ FILENAME="$NAME-$VERSION.zip"
 echo "Building $FILENAME"
 if [ -f install/$FILENAME ]; then
   echo -n "Installl file for this version already exists, overwrite (y/N): "
-  read line;
-  if [ x"$line" != x"y" -o x"$line" != x"Y" ]; then
+  read CHAR;
+  if [ x"$CHAR" != x"y" -a x"$CHAR" != x"Y" ]; then
     exit;
   fi
 fi
 
 echo minify and join some files
+# remove all script tags with a src= attribute
 cat index.html | sed 's/<script .* src=.*//' > index.html.temp
+# build min.js of all scripts with a src= attribute
 testing/buildMin.sh 2> /dev/null
+# inline content from <link rel="stylesheet"> tags
 testing/inline-css-to-html.php index.html.temp index-joined.html >/dev/null
 rm index.html.temp
+# append javascript/min.js to our new index
 echo "<script type='text/javascript'>" >> index-joined.html
 cat javascript/min.js >> index-joined.html
 echo "</script>" >> index-joined.html
@@ -79,12 +92,12 @@ if [ $IS_SVN_RELEASE -eq 0 ];then
   # this is a release, use the joined version as main index.html
   mv index.html index.dev.html
   mv index-joined.html index.html
-  REVERT_INDEX=1
 fi
 
-# if the testdb was orrigionally a sym-link, preserve
-# that sym-link
+# if the testdb was orrigionally a sym-link, remove for package building,
+# but preserve the link aftewards
 TESTDB='protected/data/source-test.db'
+LINK=''
 if [ -h $TESTDB ]; then
   LINK=$(readlink $TESTDB)
 fi
@@ -92,13 +105,6 @@ fi
 echo generate the initial database
 rm -f $TESTDB
 cd protected/data && ./genOrig.sh && cd - >/dev/null
-
-# regenerate the sym-link
-if [ x"$LINK" != x"" ];then
-  cp $TESTDB $LINK
-  rm -f $TESTDB
-  ln -s $LINK $TESTDB
-fi
 
 # Used to build installation zip
 EXSTRING='--exclude-vcs'
@@ -111,6 +117,15 @@ rm install/*.tar
 tar -cf install/$NAME.tar . $EXSTRING && cd install && \
 echo "building outer zip archive" && zip $FILENAME * -x \*.zip
 
+ZIP_SUCESS=$?
+
+# regenerate the test db sym-link if needed
+if [ x"$LINK" != x"" ];then
+  cp $TESTDB $LINK
+  rm -f $TESTDB
+  ln -s $LINK $TESTDB
+fi
+
 # revert changes made to appinfo.json
 if [ 1 -eq $IS_SVN_RELEASE ]; then
   svn revert ../appinfo.json >/dev/null
@@ -120,7 +135,7 @@ else
 fi
 
 
-if [ $? != 0 ]; then
+if [ ! ZIP_SUCCESS -eq 0 ]; then
   echo "Build Failed"
   exit 1
 fi
@@ -156,7 +171,7 @@ fi
 
 if [ x"$CHAR" = x"u" -o x"$CHAR" = x"U" ];then
   echo Uploading
-#  echo "put \"$FILENAME\"" | lftp $LFTP_NET_BOOKMARK
+  echo "put \"$FILENAME\"" | lftp $LFTP_NET_BOOKMARK
 
   echo Updating latest.php
   mkdir /tmp/buildinstall.$$
@@ -166,10 +181,9 @@ if [ x"$CHAR" = x"u" -o x"$CHAR" = x"U" ];then
   if [ $IS_SVN_RELEASE -eq 0 ]; then
     MARKER='// RELEASE'
   fi
-  cat latest.php  | sed "s|.*$MARKER|  echo '$VERSION'; $MARKER|" | tee > latest.php.new
-#  mv latest.php.new latest.php
-#  echo "cd ..;put latest.php" | lftp $LFTP_NET_BOOKMARK
-  cd - && rm $FILENAME
+  cat latest.php  | sed "s|.*$MARKER|  echo '$VERSION'; $MARKER|" > latest.php.new
+  mv latest.php.new latest.php
+  echo "cd ..;put latest.php" | lftp $LFTP_NET_BOOKMARK
 
   echo "Now available As: "
   echo "http://nmtdvr.com/downloads/$NAME-$VERSION.zip"
