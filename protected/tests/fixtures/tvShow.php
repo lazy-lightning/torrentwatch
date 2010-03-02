47e9b1c50bf2b9a50f40623e1f5467279fadf7ec
somedude119 <somedude119@84144212-d151-0410-ac56-97821e5b5ab9>
2010-03-02 02:32:33 +0000

    functional testing for tvEpisode inspector and tvEpisode load feed items

--- protected/tests/fixtures/tvShow.php
+++ protected/tests/fixtures/tvShow.php
@@ -1,5 +1,5 @@
 <?php
 return array(
-    'sample'    => array('title'=>'Sample TV Show'),
-    'sampletwo' => array('title'=>'Other TV Show'),
+    'sample'    => array('title'=>'Sample TV Show', 'description'=>'Some information about the show'),
+    'sampletwo' => array('title'=>'Other TV Show', 'description'=>'blah blah this is a description'),
 );
