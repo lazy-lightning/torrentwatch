1270dcbcc73df03d74500e4f22048d600f79d033
somedude119 <somedude119@84144212-d151-0410-ac56-97821e5b5ab9>
2010-03-17 23:44:28 +0000

    create a TVDbAdapter much like IMDbAdapter. this will allow other adapters to be plugged in simplify updateTVDBCommand by using TVDbAdapter

--- protected/commands/updateTVDBCommand.php
+++ protected/commands/updateTVDBCommand.php
@@ -2,152 +2,153 @@
 
 class updateTVDBCommand extends BaseConsoleCommand {
 
-  // array of loaded tvShows indexed by tvdbid
-  private $tvShows = array();
+  /**
+    * @var TVDbAdapter
+    */
+  protected $details;
+
+  /**
+    * @var modelFactory
+    */
   protected $factory;
 
+  /**
+   * toSave 
+   * 
+   * @var array
+   */
+  protected $toSave = array();
+
+  /**
+   * scanned 
+   * 
+   * @var array
+   */
+  private $scanned = array();
+
+  /**
+   * tvShowRescanTime 
+   * 
+   * @var integer the time in secconds to wait before rescanning tvdb for data when no info is found
+   *              default of 172800 makes for 48 hours.
+   */
+  public $tvShowRescanTime = 172800;
+
+  /**
+   * run 
+   * 
+   * @param mixed $args 
+   * @return void
+   */
   public function run($args) {
-    require_once('TVDB.php');
     $this->factory = Yii::app()->modelFactory;
-    $this->updateTvShows();
-    $this->updateTvEpisodes();
-  }
+    $this->details = new TVDbAdapter($this->factory);
 
-  protected function updateTvEpisodes() {
-    $db = Yii::app()->db;
-    $now=time();
-    $scanned = $toSave = array();
-    $reader = $db->createCommand('SELECT e.id id, e.season season, e.episode episode, s.tvdbId tvdbId'.
-                                 '  FROM tvShow s,'.
-                                 '       ( SELECT * FROM tvEpisode e'.
-                                 '          WHERE e.description IS NULL'.
-                                 '            AND e.lastTvdbUpdate < '.($now-(3600*48)).
-                                 '       ) e'.
-                                 ' WHERE s.tvdbId NOT NULL'.
-                                 '   AND s.id = e.tvShow_id'
+    $toScan = array(
+        'tvShow' => array(
+          'command' => $this->getScanTvShowsCommand(),
+          'get'     => array($this, 'getTvShowScraper'),
+          'update'  => array($this->details, 'updateTvShowFromScraper'),
+        ),
+        'tvEpisode' => array(
+          'command' => $this->getScanTvEpisodesCommand(),
+          'get'     => array($this, 'getTvEpisodeScraper'),
+          'update'  => array($this->details, 'updateTvEpisodeFromScraper'),
+        ),
+    );
 
-    )->queryAll();
-    foreach($reader as $row) {
-      $scanned[] = $row['id'];
-      // Dont have the class functions written for date based episode
-      if($row['episode'] > 1000) {
-        continue;
-      }
-
-      echo "Looking for tvdbId ".$row['tvdbId']."\n";
-      if(!isset($this->tvShows[$row['tvdbId']])) {
-        $this->tvShows[$row['tvdbId']] = TV_Shows::findById($row['tvdbId']);
-      }
-
-      $tvdbShow = $this->tvShows[$row['tvdbId']];
-
-      if(!$tvdbShow) {
-        continue;
-      }
-
-      echo "Looking for episode ".$row['season']."x".$row['episode']."\n";
-      $ep = $tvdbShow->getEpisode($row['season'], $row['episode']);
-      if(!$ep) {
-        continue;
-      }
-
-      echo "Found! Updating ".$ep->name."\n";
-      $tvEpisode = tvEpisode::model()->findByPk($row['id']);
-      $tvEpisode->firstAired = $ep->firstAired;
-      $tvEpisode->description = $ep->overview;
-      $tvEpisode->title = $ep->name;
-      $toSave[] = $tvEpisode;
-    }
-    $transaction = Yii::app()->db->beginTransaction();
-    try {
-      echo count($toSave)." tv Episodes to save\n";
-      foreach($toSave as $record)
-        $record->save();
-      if(count($scanned))
-        tvEpisode::model()->updateByPk($scanned, array('lastTvdbUpdate'=>$now));
-      $transaction->commit();
-    } catch ( Exception $e ) {
-      $transaction->rollback();
-    }
-  }
-
-  protected function updateTvShows() {
-    $db = Yii::app()->db;
-    $scanned = $toSave = array();
-    $now = time();
-
-    $reader = $db->createCommand('SELECT id,title FROM tvShow'.
-                                 ' WHERE description IS NULL'.
-                                 '   AND lastTvdbUpdate < '.($now-(3600*48))
-    )->queryAll();
-    foreach($reader as $row) 
+    foreach($toScan as $class => $config)
     {
-      $scanned[]= $row['id'];
-      echo "Searching for ".$row['title']."\n";
-      $tvdbShows = TV_Shows::search($row['title']);
-      if(!$tvdbShows)
-        continue;
-
-      $data = $tvdbShows[0];
-      echo "Found data for ".$data->seriesName."\n";
-      $this->tvShows[$data->tvdbId] = $data;
-
-      $toSave[$row['id']] = $data;
-    }
-
-    echo count($toSave)." tv Shows to save\n";
-    $transaction = $db->beginTransaction();
-    try {
-      foreach($toSave as $id => $data)
-      {
-        $tvShow = tvShow::model()->findByPk($id);
-        // Dont change the title will mess up $this->factory->tvShowByTitle()
-        // perhaps create new column for tvdb Name ?
-        //$tvShow->title = $data->seriesName;
-
-        if(!empty($data->network))
-          $tvShow->network_id = $this->factory->networkByTitle($data->network)->id;
-        $tvShow->rating = (integer) $data->rating;
-        $tvShow->description = $data->overview;
-        $tvShow->tvdbId = $data->id;
-  
-        if($tvShow->save()) 
-        {
-          echo "Saved {$tvShow->title}\n";
-          if(!empty($data->genres)) 
-          {
-            // Initialize our SQL INSERT command
-            $genre_id = '';
-            if(!isset($addGenre)) 
-            {
-              $addGenre = $db->createCommand(
-                  "INSERT INTO tvShow_genre (tvShow_id, genre_id) VALUES (:tvShow, :genre);"
-              );
-              $addGenre->bindParam(':genre', $genre_id);
-            }
-            $addGenre->bindValue(':tvShow', $tvShow->id);
-    
-            // Loopthrough the genres linking them all to the tvShow
-            foreach($data->genres as $genre) 
-            {
-              $genre_id = $this->factory->genreByTitle($genre)->id;
-              $addGenre->execute();
-            }
-          }
-        } 
-        else 
-        {
-          echo "Failed save {$tvShow->title}\n";
-          Yii::log('Error saving tvShow after tvdb update', CLogger::LEVEL_ERROR);
-          Yii::log(print_r($tvShow->errors, TRUE));
-        }
+      $this->scan($class, $config['command'], $config['get']);
+      $transaction = Yii::app()->getDb()->beginTransaction();
+      try {
+        $this->updateDatabase($class, $config['update']);
+        $transaction->commit();
+      } catch (Exception $e) {
+        $transaction->rollback();
+        throw $e;
       }
-      if(count($scanned))
-        tvShow::model()->updateByPk($scanned, array('lastTvdbUpdate'=>$now));
-      $transaction->commit();
-    } catch ( Exception $e ) {
-      $transaction->rollback();
-      throw $e;
+    }
+  }
+
+  protected function getScanTvEpisodesCommand()
+  {
+    return Yii::app()->getDb()->createCommand(<<<EOD
+SELECT e.id id, e.season season, e.episode episode, s.tvdbId tvdbId, s.title as tvShow_title
+  FROM tvShow s,
+       ( SELECT * FROM tvEpisode e
+          WHERE e.description IS NULL
+          AND e.lastTvdbUpdate < :rescanTime
+       ) e
+ WHERE s.tvdbId NOT NULL
+ AND e.episode < 10000
+ AND s.id = e.tvShow_id
+EOD
+    )->bindValue(':rescanTime', time()-$this->tvShowRescanTime);
+  }
+
+  public function getScanTvShowsCommand()
+  {
+    return Yii::app()->getDb()->createCommand(<<<EOD
+SELECT id,title
+  FROM tvShow
+ WHERE description IS NULL
+   AND lastTvdbUpdate < :rescanTime
+EOD
+    )->bindValue(':rescanTime', time()-$this->tvShowRescanTime);
+  }
+
+  protected function getTvEpisodeScraper($resultRow)
+  {
+    echo "Looking for {$resultRow['tvShow_title']}: Season {$resultRow['season']}, Episode {$resultRow['episode']}\n";
+    return $this->details->getTvEpisodeScraper($resultRow['tvdbId'], $resultRow['season'], $resultRow['episode']);
+  }
+
+  protected function getTvShowScraper($resultRow)
+  {
+    echo "Looking for {$resultRow['title']}\n";
+    return $this->details->getTvShowScraperByTitle($resultRow['title']);
+  }
+
+  protected function scan($class, $command, $getScraperCallback)
+  {
+    $scanned = array();
+    foreach($command->queryAll() as $row) {
+      $this->scanned[] = $row['id'];
+      $scraper = call_user_func($getScraperCallback, $row);
+      if($scraper)
+      {
+        echo "Found! Will update ".$scraper->name."\n";
+        $this->toSave[$row['id']] = $scraper;
+      }
+    }
+  }
+
+  protected function updateDatabase($class, $updateFromScraperCallback)
+  {
+    $model = CActiveRecord::model($class);
+    if(count($this->scanned))
+    {
+      echo "Marking ".count($this->scanned)." {$class}s as scanned ...";
+      $model->updateByPk($this->scanned, array('lastTvdbUpdate'=>time()));
+      $this->scanned = array();
+      echo "Done.\n";
+    }
+    if(count($this->toSave))
+    {
+      $saved = 0;
+      foreach($this->toSave as $id => $scraper)
+      {
+        echo "Saving {$scraper->name} ...";
+        $record = $model->findByPk($id);
+        if($record && call_user_func($updateFromScraperCallback, $record, $scraper))
+          ++$saved;
+        else
+          echo "Failed";
+        echo "\n";
+      }
+      $this->toSave = array();
+      echo "Saved $saved {$class}s\n";
     }
   }
 }
